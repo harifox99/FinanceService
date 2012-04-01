e8d1c8f65f03eb255db5e8afeb4ec2208080db6e
bear <aluba0504@gmail.com>
2012-04-01 02:55:17 +0000

    修改擷取資料之程式

--- src/org/bear/datainput/ImportBalanceSheetCathay.java
+++ src/org/bear/datainput/ImportBalanceSheetCathay.java
@@ -15,44 +15,26 @@ public class ImportBalanceSheetCathay extends ImportStockIDData
 	{
 		try
 		{		
-			int idleTime = 0;
-			for (int j = 0; j < wrapperList.size(); j++)
+			String[] seasons = {"01", "02", "03", "04"};
+			for (int i = 0; i < seasons.length; i++)
 			{
-				String stockID = wrapperList.get(j).getStockID();
-				System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");				
-				//年資料
-				/*
-				GetURLCathayBalanceSheet urlContent = new GetURLCathayBalanceSheet(stockID, true);
-				BalanceSheetParserCathay balanceSheetYear = new BalanceSheetParserCathay(urlContent.getContent(), stockID, true);
-				balanceSheetYear.parse(2);
-				//季資料*/
-				GetURLCathayBalanceSheet urlContent = new GetURLCathayBalanceSheet(stockID, false);
-				BalanceSheetParserCathay balanceSheetSeason = new BalanceSheetParserCathay(urlContent.getContent(), stockID, false, "2011", "01");
-				balanceSheetSeason.parse(2);
-				Thread.sleep(10000);		
-				idleTime++;
-			}
-			for (int j = 0; j < wrapperList.size(); j++)
-			{
-				String stockID = wrapperList.get(j).getStockID();
-				System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");				
-				//季資料*/
-				GetURLCathayBalanceSheet urlContent = new GetURLCathayBalanceSheet(stockID, false);
-				BalanceSheetParserCathay balanceSheetSeason = new BalanceSheetParserCathay(urlContent.getContent(), stockID, false, "2011", "02");
-				balanceSheetSeason.parse(2);
-				Thread.sleep(10000);		
-				idleTime++;
-			}
-			for (int j = 0; j < wrapperList.size(); j++)
-			{
-				String stockID = wrapperList.get(j).getStockID();
-				System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");				
-				//季資料*/
-				GetURLCathayBalanceSheet urlContent = new GetURLCathayBalanceSheet(stockID, false);
-				BalanceSheetParserCathay balanceSheetSeason = new BalanceSheetParserCathay(urlContent.getContent(), stockID, false, "2011", "03");
-				balanceSheetSeason.parse(2);
-				Thread.sleep(10000);		
-				idleTime++;
+				int idleTime = 0;
+				for (int j = 0; j < wrapperList.size(); j++)
+				{
+					String stockID = wrapperList.get(j).getStockID();
+					System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");				
+					//年資料
+					/*
+					GetURLCathayBalanceSheet urlContent = new GetURLCathayBalanceSheet(stockID, true);
+					BalanceSheetParserCathay balanceSheetYear = new BalanceSheetParserCathay(urlContent.getContent(), stockID, true);
+					balanceSheetYear.parse(2);
+					//季資料*/
+					GetURLCathayBalanceSheet urlContent = new GetURLCathayBalanceSheet(stockID, false);
+					BalanceSheetParserCathay balanceSheetSeason = new BalanceSheetParserCathay(urlContent.getContent(), stockID, false, "2010", seasons[i]);
+					balanceSheetSeason.parse(2);
+					Thread.sleep(10000);		
+					idleTime++;
+				}			
 			}
 		}
 		catch (Exception ex)
