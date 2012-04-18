61172583ddb41bfab7feef63723874738c16c82c
bear <aluba0504@gmail.com>
2012-04-18 09:36:02 +0000

    修改年份與季節資料處理模式

--- src/org/bear/datainput/ImportIncomeStatementCathay.java
+++ src/org/bear/datainput/ImportIncomeStatementCathay.java
@@ -16,44 +16,27 @@ public class ImportIncomeStatementCathay extends ImportStockIDData
 	{
 		try
 		{		
-			int idleTime = 0;
-			for (int j = 0; j < wrapperList.size(); j++)
-			{
-				String stockID = wrapperList.get(j).getStockID();
-				System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");
-				//年資料
-				/*
-				GetURLCathayIncomeStatement urlContent = new GetURLCathayIncomeStatement(stockID, true);
-				IncomeStatementParserCathay incomeStatementYear = new IncomeStatementParserCathay(urlContent.getContent(), stockID, true, "99", "00");
-				incomeStatementYear.parse(2);*/
-				//季資料
-				GetURLCathayIncomeStatement urlContent = new GetURLCathayIncomeStatement(stockID, false);
-				IncomeStatementParserCathay incomeStatementSeason = new IncomeStatementParserCathay(urlContent.getContent(), stockID, false, "2010", "01");
-				incomeStatementSeason.parse(2);
-				Thread.sleep(10000);	
-				idleTime++;
-			}
-			for (int j = 0; j < wrapperList.size(); j++)
-			{
-				String stockID = wrapperList.get(j).getStockID();
-				System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");
-				//季資料
-				GetURLCathayIncomeStatement urlContent = new GetURLCathayIncomeStatement(stockID, false);
-				IncomeStatementParserCathay incomeStatementSeason = new IncomeStatementParserCathay(urlContent.getContent(), stockID, false, "2010", "02");
-				incomeStatementSeason.parse(2);
-				Thread.sleep(10000);	
-				idleTime++;
-			}
-			for (int j = 0; j < wrapperList.size(); j++)
-			{
-				String stockID = wrapperList.get(j).getStockID();
-				System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");
-				//季資料
-				GetURLCathayIncomeStatement urlContent = new GetURLCathayIncomeStatement(stockID, false);
-				IncomeStatementParserCathay incomeStatementSeason = new IncomeStatementParserCathay(urlContent.getContent(), stockID, false, "2010", "03");
-				incomeStatementSeason.parse(2);
-				Thread.sleep(10000);	
-				idleTime++;
+			//String[] seasons = {"04"};
+			String year[] = {"2011"};
+			for (int i = 0; i < year.length; i++)
+			{			
+				int idleTime = 0;
+				for (int j = 0; j < wrapperList.size(); j++)
+				{
+					String stockID = wrapperList.get(j).getStockID();
+					System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");
+					//年資料
+					GetURLCathayIncomeStatement urlContent = new GetURLCathayIncomeStatement(stockID, true);
+					IncomeStatementParserCathay incomeStatementYear = new IncomeStatementParserCathay(urlContent.getContent(), stockID, true, year[i], "00");
+					incomeStatementYear.parse(2);
+					//季資料
+					/*
+					GetURLCathayIncomeStatement urlContent = new GetURLCathayIncomeStatement(stockID, false);
+					IncomeStatementParserCathay incomeStatementSeason = new IncomeStatementParserCathay(urlContent.getContent(), stockID, false, "2011", "04");
+					incomeStatementSeason.parse(2);*/
+					Thread.sleep(10000);	
+					idleTime++;
+				}
 			}
 		}
 		catch (Exception ex)
