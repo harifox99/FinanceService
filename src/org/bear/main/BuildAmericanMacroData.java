b73c6d1765f795f03a76a9539e4063cfa86796e4
bear <aluba0504@gmail.com>
2013-06-08 13:18:59 +0000

    可個別更新FRED的總經資料

--- src/org/bear/main/BuildAmericanMacroData.java
+++ src/org/bear/main/BuildAmericanMacroData.java
@@ -1,10 +1,15 @@
 package org.bear.main;
-
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import org.bear.constant.FredIndexParameterMap;
 import org.bear.dao.AmericanMacroPantherDao;
 import org.bear.datainput.ImportAmericanMacroPanther;
 import org.bear.entity.AmericanMacroPantherEntity;
+import org.bear.parser.fred.FredEconomicUrl;
 import org.bear.util.ParseFile;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -18,8 +23,78 @@ public class BuildAmericanMacroData extends ParseFile
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		new BuildAmericanMacroData().insertBatch();
+		//new BuildAmericanMacroData().insertBatch();
 		//new BuildMacroEconomicData().findAllList();
+		FredEconomicUrl type = new FredEconomicUrl();
+		new BuildAmericanMacroData().insertByFredApi(FredIndexParameterMap.AMERICA_LIST, type, FredIndexParameterMap.HOUST);
+		new BuildAmericanMacroData().insertByFredApi(FredIndexParameterMap.AMERICA_PAYEMS_LIST, type, FredIndexParameterMap.PAYEMS);
+		new BuildAmericanMacroData().insertByFredApi(FredIndexParameterMap.AMERICA_ICSA_LIST, type, FredIndexParameterMap.ICSA);
+		new BuildAmericanMacroData().insertByFredApi(FredIndexParameterMap.AMERICA_INTEREST_LIST, type, FredIndexParameterMap.INTEREST);
+		new BuildAmericanMacroData().insertByFredApi(FredIndexParameterMap.AMERICA_YOY_LIST, type, FredIndexParameterMap.CPIAUCSL);
+		new BuildAmericanMacroData().insertByFredApi(FredIndexParameterMap.AMERICA_MONEY_LIST, type, FredIndexParameterMap.MONEY);
+	}
+	public void insertByFredApi(String[] catagoryList, FredEconomicUrl catagory, HashMap<String, String> parameterMap)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		AmericanMacroPantherDao dao = (AmericanMacroPantherDao)context.getBean("americanMacroPantherDao");
+		String key = "date";
+		String value = "value";
+		list = new ArrayList<AmericanMacroPantherEntity>();
+		try
+		{
+			for (int i = 0; i < catagoryList.length; i++)
+			{
+				catagory.setUrlString(catagoryList[i], catagory.observation_init, parameterMap);
+				/*
+				SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-DD");
+				String strDate = bufferData[0].trim() + "-" + bufferData[1].trim();
+				Date date = dateFormat.parse(strDate);
+				entity.setYearMonth(date);
+				entity.setYear(bufferData[0].trim());
+				entity.setDate(strDate);*/
+				HashMap<String, String> map = catagory.getContent(key, value);
+				//建第一欄資料與日期
+				if (catagoryList[i].equals("HOUST"))
+				{
+					for (Map.Entry<String,String> entry : map.entrySet()) 
+					{
+						AmericanMacroPantherEntity entity = new AmericanMacroPantherEntity();
+						String dateArray[] = entry.getKey().split("-");
+						SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+						Date date = dateFormat.parse(entry.getKey());
+						entity.setDate(entry.getKey());
+						entity.setYear(dateArray[0]);
+						entity.setYearMonth(date);
+						entity.setHoust(entry.getValue());
+						list.add(entity);
+					}
+					dao.insertBatch(list);
+				}
+				//之後用日期當Key，更新其他欄位資料
+				else
+				{
+					for (Map.Entry<String,String> entry : map.entrySet()) 
+					{				
+						int result;
+						if (catagoryList[i].equals("CPIAUCSL"))
+							result = dao.update("CPI", entry.getValue(), entry.getKey());
+						else
+							result = dao.update(catagoryList[i], entry.getValue(), entry.getKey());
+					    if (result <= 0)
+					    {					    	
+					    	System.out.println(entry.getValue() + ", " + entry.getKey());
+					    	//System.out.println("Abnormal Terminating!");
+					    	//System.exit(1);
+					    }
+					}
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
 	}
 	public void insertBatch()
 	{
