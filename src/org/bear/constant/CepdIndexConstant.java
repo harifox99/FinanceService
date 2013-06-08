44f0534b69418d3e062d7b037f70520a12338408
bear <aluba0504@gmail.com>
2013-06-08 06:51:51 +0000

    經建會資訊擷取常數

---
+++ src/org/bear/constant/CepdIndexConstant.java
@@ -0,0 +1,8 @@
+package org.bear.constant;
+
+public class CepdIndexConstant {
+	/* 景氣領先指標綜合指數, 六個月領先指標, 製造業存貨量指數, 工業及服務業每人每月加班工時, SEMI半導體接單出貨比, 景氣對策信號  */
+	public static final String[] CEPD_LIST = {"SR0001", "SR0002", "SR0010", "SR0011", "SR0013", "SR0005"};
+	public static final String[] CEPD_NAME = {"GeneralIndex", "SixMonthLeadIndex", "InventoryIndex", "OverworkTime", "SemiIndex", "LightSignal"};
+	public static final String[] CEPD_MAP = {"it01", "it01", "it01", "it01", "it01", "it02"};
+}
