1cd3e08b8803e42b8adbc24c5828391170edb15f
bear <aluba0504@gmail.com>
2013-06-08 06:50:42 +0000

    央行資訊擷取常數

---
+++ src/org/bear/constant/CbcIndexConstant.java
@@ -0,0 +1,19 @@
+package org.bear.constant;
+
+import java.util.HashMap;
+
+public class CbcIndexConstant 
+{
+	/* ¤é´Á */
+	public static final HashMap<String, Integer> MONTH_HASH = new HashMap<String, Integer>();
+	static
+	{
+		MONTH_HASH.put("1993M02", 380);
+		MONTH_HASH.put("2013M03", 621);
+		MONTH_HASH.put("2013M01", 619);
+	}	
+	
+	/* ³f¹ô */
+	public static final String[] MONEY = {"M1bAverage", "M1bEnd", "M2Average", "M2End"};
+	
+}
