2d362bc56cdb56efe64d22bae429ed1877a70e40
bear <aluba0504@gmail.com>
2013-06-08 11:51:23 +0000

    解析央行的總經資料(CBC)

---
+++ src/org/bear/parser/taiwanMacro/CbcParser.java
@@ -0,0 +1,68 @@
+package org.bear.parser.taiwanMacro;
+import java.util.List;
+import org.bear.constant.CbcIndexConstant;
+import org.bear.dao.MacroEconomicDao;
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.HTMLElementName;
+import net.htmlparser.jericho.Source;
+/**
+ * 解析央行的總經資料(CBC)
+ * @author edward
+ *
+ */
+public class CbcParser 
+{
+	MacroEconomicDao dao;
+	public String responseString;
+	List<Element> elementList = null;
+	public String getResponseString() {
+		return responseString;
+	}
+	public void setResponseString(String responseString) {
+		this.responseString = responseString;
+	}
+	public void setDao(MacroEconomicDao dao) {
+		this.dao = dao;
+	}
+	public void getTableContent(Element element) 
+	{
+		List<Element> trList = element.getAllElements(HTMLElementName.TR);
+		for (int i = 0; i < trList.size()-1; i++)
+		{
+			Element trElement = trList.get(i);
+			List<Element> tdList = trElement.getAllElements(HTMLElementName.TD);
+			Element resultElement = null;
+			String dateString = null;
+			for (int j = 0; j < tdList.size(); j++)
+			{				
+				resultElement = tdList.get(j);
+				String content = resultElement.getContent().toString().trim();
+				try
+				{
+					if (j == 0)
+					{
+						dateString = content;
+					}
+					else
+					{
+						int result = dao.update(CbcIndexConstant.MONEY[j-1], content, dateString, "M");
+						if (result <= 0)
+					    {					    	
+					    	System.out.println(content + ", " + dateString);
+					    }
+					}
+				}
+				catch (Exception ex)
+				{
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	public void parse(int tableIndex) {
+		Source source = new Source(responseString);
+		//System.out.println(source.toString());
+		elementList = source.getAllElements(HTMLElementName.TABLE);
+		this.getTableContent(elementList.get(tableIndex));				
+	}
+}
