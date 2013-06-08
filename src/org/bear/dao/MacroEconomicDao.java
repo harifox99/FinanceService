cdc929e6a0ea191bec9bc2c427f6a9c4b8a556ad
bear <aluba0504@gmail.com>
2013-06-08 07:09:28 +0000

    可部分更新台灣總經資料庫資料

--- src/org/bear/dao/MacroEconomicDao.java
+++ src/org/bear/dao/MacroEconomicDao.java
@@ -7,4 +7,5 @@ public interface MacroEconomicDao
 	public void insertBatch(List <MacroEconomicEntity>  entity);
 	public List <MacroEconomicEntity> findAll();
 	public List <MacroEconomicEntity> findByDate(Date startTime, Date endTime);
+	public int update(String indexName, String indexValue, String date, String split);
 }
