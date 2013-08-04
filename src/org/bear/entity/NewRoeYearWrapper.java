b7ecb90d4f54d32b466b565c0ce7d554820fe246
bear <aluba0504@gmail.com>
2013-08-04 11:16:45 +0000

    計算[連續年度ROE期望分析]，第一版

---
+++ src/org/bear/entity/NewRoeYearWrapper.java
@@ -0,0 +1,96 @@
+package org.bear.entity;
+
+public class NewRoeYearWrapper {
+	String stockID;
+	double _2002;
+	double _2003;
+	double _2004;
+	double _2005;
+	double _2006;
+	double _2007;
+	double _2008;
+	double _2009;
+	double _2010;
+	double _2012;
+	double _2013;
+	double _2014;
+	public String getStockID() {
+		return stockID;
+	}
+	public void setStockID(String stockID) {
+		this.stockID = stockID;
+	}
+	public double get_2002() {
+		return _2002;
+	}
+	public void set_2002(double _2002) {
+		this._2002 = _2002;
+	}
+	public double get_2003() {
+		return _2003;
+	}
+	public void set_2003(double _2003) {
+		this._2003 = _2003;
+	}
+	public double get_2004() {
+		return _2004;
+	}
+	public void set_2004(double _2004) {
+		this._2004 = _2004;
+	}
+	public double get_2005() {
+		return _2005;
+	}
+	public void set_2005(double _2005) {
+		this._2005 = _2005;
+	}
+	public double get_2006() {
+		return _2006;
+	}
+	public void set_2006(double _2006) {
+		this._2006 = _2006;
+	}
+	public double get_2007() {
+		return _2007;
+	}
+	public void set_2007(double _2007) {
+		this._2007 = _2007;
+	}
+	public double get_2008() {
+		return _2008;
+	}
+	public void set_2008(double _2008) {
+		this._2008 = _2008;
+	}
+	public double get_2009() {
+		return _2009;
+	}
+	public void set_2009(double _2009) {
+		this._2009 = _2009;
+	}
+	public double get_2010() {
+		return _2010;
+	}
+	public void set_2010(double _2010) {
+		this._2010 = _2010;
+	}
+	public double get_2012() {
+		return _2012;
+	}
+	public void set_2012(double _2012) {
+		this._2012 = _2012;
+	}
+	public double get_2013() {
+		return _2013;
+	}
+	public void set_2013(double _2013) {
+		this._2013 = _2013;
+	}
+	public double get_2014() {
+		return _2014;
+	}
+	public void set_2014(double _2014) {
+		this._2014 = _2014;
+	}
+	
+}
