f043e2b502e49d68729947892842765b2e9a9ab6
bear <aluba0504@gmail.com>
2013-08-04 13:12:44 +0000

    儲存[連續年度ROE期望分析]的Wrapper

---
+++ src/org/bear/entity/RoeYearWrapper.java
@@ -0,0 +1,35 @@
+package org.bear.entity;
+
+import java.util.List;
+
+public class RoeYearWrapper {
+	String stockID;
+	List<Double> roeList;
+	int roeSize;
+	String year;
+	public String getStockID() {
+		return stockID;
+	}
+	public void setStockID(String stockID) {
+		this.stockID = stockID;
+	}
+	public int getRoeSize() {
+		return roeSize;
+	}
+	public void setRoeSize(int roeSize) {
+		this.roeSize = roeSize;
+	}
+	public List<Double> getRoeList() {
+		return roeList;
+	}
+	public void setRoeList(List<Double> roeList) {
+		this.roeList = roeList;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	
+}
