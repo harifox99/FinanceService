a1a7b193222c693994f44b7052feaf48c07a63fc
bear <aluba0504@gmail.com>
2013-08-04 13:16:36 +0000

    計算[連續年度ROE期望分析]

---
+++ src/org/bear/financeAnalysis/RoeAnalysis.java
@@ -0,0 +1,90 @@
+package org.bear.financeAnalysis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bear.dao.BalanceSheetDao;
+import org.bear.dao.BasicStockDao;
+import org.bear.dao.IncomeStatementDao;
+import org.bear.entity.RoeYearWrapper;
+import org.bear.entity.BalanceSheetEntity;
+import org.bear.entity.BasicStockWrapper;
+import org.bear.entity.IncomeStatementEntity;
+import org.bear.util.StringUtil;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class RoeAnalysis 
+{
+	ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+	BasicStockDao dao = (BasicStockDao)context.getBean("basicStockDao");
+	FinanceUtil financeUtil;
+	//資產負債表
+	BalanceSheetDao balanceSheetDao = (BalanceSheetDao)context.getBean("basicBalanceSheetDao");
+	List <BalanceSheetEntity> balanceSheetList;
+	//損益表
+	IncomeStatementDao incomeStatementDao = (IncomeStatementDao)context.getBean("basicIncomeStatementDao");
+	List <IncomeStatementEntity> incomeStatementList;
+	/**
+	 * 用此程式來篩選，過去N個年度，期望至少有M年，符合期望的ROE，例如：
+	 * 希望過去10年，有8年，其期望的ROE > 15%，則
+	 * totalYear = 10, demandYear = 8, demandRoe = 15;
+	 * **********************************************
+	 * 注意，這裡ROE的算法和[ROE細部分析]的方法略有不同
+	 * @param totalYear 查詢過去N年度的ROE
+	 * @param demandYear 期望有幾個年度符合需求
+	 * @param demandRoe 期望ROE
+	 * @return
+	 */
+	public List<RoeYearWrapper> getDemandROEList(int totalYear, int demandYear, int demandRoe)
+	{
+		List<BasicStockWrapper> stockList = dao.findAllData();
+		List<RoeYearWrapper> roeYearList = new ArrayList<RoeYearWrapper>();
+		for (int i = 0; i < stockList.size(); i++)
+		{
+			String stockID = "";
+			stockID = stockList.get(i).getStockID();
+			//if (stockID.equals("1232"))
+				//System.out.println();
+			balanceSheetList = balanceSheetDao.findDataByYear(stockID);
+			incomeStatementList = incomeStatementDao.findDataByYear(stockID);
+			if (incomeStatementList.size() != balanceSheetList.size())
+				continue;
+			if (incomeStatementList.size() < totalYear)
+				continue;
+			RoeYearWrapper wrapper = new RoeYearWrapper();
+			wrapper.setRoeSize(totalYear);
+			List<Double> roeList = new ArrayList<Double>();
+			for (int j = incomeStatementList.size() - totalYear; j < incomeStatementList.size(); j++)
+			{
+				if (j == incomeStatementList.size() - totalYear)
+				{
+					wrapper.setStockID(stockID);
+					wrapper.setYear(incomeStatementList.get(j).getYear());
+				}
+				double roe = (double)incomeStatementList.get(j).getNetIncome() / balanceSheetList.get(j).getStockholdersEquity();				
+				roeList.add(StringUtil.setPointLength(roe*100));
+			}			
+			wrapper.setRoeList(roeList);
+			if (this.checkExpectedRoe(roeList, demandRoe, demandYear))
+				roeYearList.add(wrapper);
+		}
+		return roeYearList;
+	}
+	private boolean checkExpectedRoe(List<Double> roeList, int demandRoe, int demandYear)
+	{
+		int expectedRoeCount = 0;
+		for (int i = 0; i < roeList.size(); i++)
+		{
+			if (roeList.get(i) >= demandRoe)
+			{
+				expectedRoeCount++;
+			}
+		}
+		if (expectedRoeCount >= demandYear)
+			return true;
+		else
+			return false;
+	}
+	
+}
