7f77dfc6adc46750e3b2190cdc8339929aa987be
bear <aluba0504@gmail.com>
2013-08-19 14:50:47 +0000

    解析證券期貨發展基金會的月營收資料

---
+++ src/org/bear/parser/sfi/RevenueParser.java
@@ -0,0 +1,71 @@
+package org.bear.parser.sfi;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.HTMLElementName;
+
+import org.bear.entity.RevenueEntity;
+
+public class RevenueParser extends ParserBase
+{
+	public void getTableContent(Element element) 
+	{
+		List<Element> trList = element.getAllElements(HTMLElementName.TR);
+		for (int i = 0; i < trList.size(); i++)
+		{
+			if (i == 0)
+				continue;
+			Element trElement = trList.get(i);
+			List<Element> tdList = trElement.getAllElements(HTMLElementName.TD);
+			Element resultElement = null;
+			String year = null;
+			RevenueEntity entity = new RevenueEntity();
+			for (int j = 0; j < tdList.size(); j++)
+			{	
+				resultElement = tdList.get(j).getFirstElement(HTMLElementName.SPAN);				
+				String content = resultElement.getContent().toString().trim();				
+				//讓數字的","消失
+				content = content.replaceAll(",", "");
+				try
+				{
+					if (j == 0)//年
+					{
+						year = content;
+						entity.setStockID(stockID);
+					}
+					else if (j == 1)//月
+					{
+						SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM");						
+						Date date = dateFormat.parse(year + "-" + content);
+						entity.setYearMonth(date);
+					}
+					else if (j == 2)//盈餘
+					{
+						entity.setRevenue(Integer.parseInt(content.substring(0, content.length()-3)));
+					}
+					else if (j == 3)//上期盈餘
+					{
+						entity.setLastRevenue(Integer.parseInt(content.substring(0, content.length()-3)));
+					}
+					else if (j == 6)//累計營收
+					{
+						entity.setAccumulation(Long.parseLong(content.substring(0, content.length()-3)));
+					}
+					else if (j == 7)//上期累計營收
+					{
+						entity.setLastAccumulation(Long.parseLong(content.substring(0, content.length()-3)));
+					}
+					else
+						continue;
+				}			
+				catch (Exception ex)
+				{
+					ex.printStackTrace();
+				}				
+			}
+			dao.update(stockID, entity);
+		}
+	}
+}
