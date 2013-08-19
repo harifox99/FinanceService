86cedd6c7da97dc0b36a70b555ec2b34d6bcd544
bear <aluba0504@gmail.com>
2013-08-19 14:51:23 +0000

    解析證券期貨發展基金會的資料-Base Class

---
+++ src/org/bear/parser/sfi/ParserBase.java
@@ -0,0 +1,44 @@
+package org.bear.parser.sfi;
+
+import java.util.ArrayList;
+import java.util.List;
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.HTMLElementName;
+import net.htmlparser.jericho.Source;
+
+import org.bear.dao.RevenueDao;
+import org.bear.entity.RevenueEntity;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public abstract class ParserBase {
+	RevenueDao dao;
+	String stockID;
+	public String responseString;
+	List<Element> elementList = null;
+	public List<RevenueEntity> entityList = new ArrayList<RevenueEntity>();	
+	public ParserBase()
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		dao = (RevenueDao)context.getBean("revenueDao");
+	}
+	public String getResponseString() {
+		return responseString;
+	}
+	public void setResponseString(String responseString) {
+		this.responseString = responseString;
+	}
+	public String getStockID() {
+		return stockID;
+	}
+	public void setStockID(String stockID) {
+		this.stockID = stockID;
+	}
+	public void parse(int tableIndex) {
+		Source source = new Source(responseString);
+		//System.out.println(source.toString());
+		elementList = source.getAllElements(HTMLElementName.TABLE);
+		this.getTableContent(elementList.get(tableIndex));				
+	}
+	public abstract void getTableContent(Element element);
+}
