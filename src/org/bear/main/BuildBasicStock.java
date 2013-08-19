f85a33712b1a9c74f6bc4f14a03c5c1058d275f5
bear <aluba0504@gmail.com>
2013-08-19 14:48:18 +0000

    修改註解

--- src/org/bear/main/BuildBasicStock.java
+++ src/org/bear/main/BuildBasicStock.java
@@ -20,9 +20,10 @@ public class BuildBasicStock {
 		new BuildBasicStock().insertBatch();
 		/************
 		 * 1. 先執行BuildBasicStock，建立股票基本資訊 (砍掉股票代碼有A的)
-		 * 2. 建立營收資料> BuildRevenueYam
+		 * 2. 建立營收資料> BuildRevenueYam or XXX
 		 * 2.1 上櫃成交資料：首頁 > 上櫃股票交易資訊 > 上櫃股票統計報表 > 市場交易月報> 櫃檯買賣股票成交資料彙總表
 		 * 2.2 上市成交資料：首頁 > 統計報表 > 依統計報表查詢 > 市場交易月報> 證券交易統計表
+		 * 2.3 或證券期貨發展基金會
 		 * 3. 財務報表
 		 * 3.1  資產負債表
 		 * 3.2 損益表
