8747d24777b068f68f113f0c7b9c5c9e22e8ffe6
bear <aluba0504@gmail.com>
2013-08-21 06:22:39 +0000

    自網站擷取櫃臺的平均價格與週轉率

--- src/org/bear/util/newRevenue/GretaiIndividualIndex.java
+++ src/org/bear/util/newRevenue/GretaiIndividualIndex.java
@@ -5,6 +5,7 @@ import java.util.*;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.bear.datainput.GetSFIContent;
+import org.bear.parser.sfi.GretaiParser;
 import org.bear.util.HttpUtil;
 
 public class GretaiIndividualIndex implements GetSFIContent {
@@ -13,6 +14,7 @@ public class GretaiIndividualIndex implements GetSFIContent {
 	public void getContent(String stockID, String startYear, String startMonth,
 			String endYear, String endMonth) 
 	{
+		GretaiParser parser = new GretaiParser();
 		String url = "http://www.otc.org.tw/ch/stock/statistics/monthly/result_st44.php";
 		List<NameValuePair> paramList = new ArrayList<NameValuePair>();
 		paramList.add(new BasicNameValuePair("timestamp", "1376901040511"));
@@ -20,9 +22,9 @@ public class GretaiIndividualIndex implements GetSFIContent {
 		paramList.add(new BasicNameValuePair("yy", startYear));
 		paramList.add(new BasicNameValuePair("input_stock_code", stockID));
 		String responseString = HttpUtil.send(url, paramList, 1, "big5");
-		//parser.setResponseString(responseString);
-		//parser.setStockID(stockID);
-		//parser.parse(2);
+		parser.setResponseString(responseString);
+		parser.setStockID(stockID);
+		parser.parse(2);
 		System.out.println(responseString);	
 	}
 	public static void main(String args[])
