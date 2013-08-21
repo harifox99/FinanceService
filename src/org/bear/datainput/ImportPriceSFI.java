10b27c916d995718175416e38c3a3ccbc67853b4
bear <aluba0504@gmail.com>
2013-08-21 06:16:46 +0000

    程式可根據上市或上櫃股票種類，呼較叫不同的程式來擷取平均價與週轉率

--- src/org/bear/datainput/ImportPriceSFI.java
+++ src/org/bear/datainput/ImportPriceSFI.java
@@ -1,4 +1,8 @@
 package org.bear.datainput;
+
+import org.bear.util.newRevenue.GetTwseIndividualIndex;
+import org.bear.util.newRevenue.GretaiIndividualIndex;
+
 public class ImportPriceSFI extends ImportStockIDData
 {
 	public void insertBatchList(String startYear, String startMonth, String endYear, String endMonth, GetSFIContent sfi)
@@ -7,14 +11,18 @@ public class ImportPriceSFI extends ImportStockIDData
 		try
 		{		
 			int idleTime = 0;
-			//for (int j = 0; j < wrapperList.size(); j++)
-			for (int j = 0; j < 1; j++)
+			for (int j = 0; j < wrapperList.size(); j++)
 			{
 				String stockID = wrapperList.get(j).getStockID();				
-				//int stockBranch = wrapperList.get(j).getStockBranch();
-				System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");				
+				int stockBranch = wrapperList.get(j).getStockBranch();
+				System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");	
 				idleTime++;
-				sfi.getContent(stockID, startYear, startMonth, endYear, endMonth);
+				if (sfi instanceof GetTwseIndividualIndex && stockBranch == 2)
+					continue;
+				else if (sfi instanceof GretaiIndividualIndex && stockBranch == 1)
+					continue;
+				else
+					sfi.getContent(stockID, startYear, startMonth, endYear, endMonth);
 			}
 		}
 		catch (Exception ex)
