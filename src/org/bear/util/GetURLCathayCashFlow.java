23302c5bb3dee7e9c48c59b016a80f9e37819cae
bear <aluba0504@gmail.com>
2013-08-23 11:55:07 +0000

    現金流量表改抓玉山銀行

--- src/org/bear/util/GetURLCathayCashFlow.java
+++ src/org/bear/util/GetURLCathayCashFlow.java
@@ -7,10 +7,14 @@ public class GetURLCathayCashFlow extends GetURLContentBase
 {
 	//public final String cashFlowsUrlHeader = "http://dj.mybank.com.tw/z/zc/zc3/zc3_1101.asp.htm";
 	//public final String cashFlowsUrlHeader2 = "http://dj.mybank.com.tw/z/zc/zc3/zc3_1101.djhtml";
-	public GetURLCathayCashFlow(String stockID)
+	public GetURLCathayCashFlow(String stockID, boolean isYear)
 	{
-		urlHeader = "http://dj.mybank.com.tw/z/zc/zc3/zc3_";
-		urlFooter = ".asp.htm";
-		this.urlString = urlHeader + stockID + urlFooter;
+		urlHeaderSeason = "http://www.esunsec.com.tw/z/zc/zc3/zc3.djhtm?A=";
+		urlHeaderYear = "http://www.esunsec.com.tw/z/zc/zc3/zc3a.djhtm?A=";	
+		urlFooter = "";
+		if (isYear)
+			this.urlString = urlHeaderYear + stockID + urlFooter;
+		else
+			this.urlString = urlHeaderSeason + stockID + urlFooter;
 	}
 }
