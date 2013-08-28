84313d42d9f3e9a05bd3d95ec0b8b2ce51b813ee
bear <aluba0504@gmail.com>
2013-08-28 12:09:31 +0000

    儲存櫃臺月交易資料的開盤與收盤價

---
+++ src/org/bear/entity/GretaiPriceEntity.java
@@ -0,0 +1,18 @@
+package org.bear.entity;
+
+public class GretaiPriceEntity {
+	String openIndex;
+	String closeIndex;
+	public String getOpenIndex() {
+		return openIndex;
+	}
+	public void setOpenIndex(String openIndex) {
+		this.openIndex = openIndex;
+	}
+	public String getCloseIndex() {
+		return closeIndex;
+	}
+	public void setCloseIndex(String closeIndex) {
+		this.closeIndex = closeIndex;
+	}
+}
