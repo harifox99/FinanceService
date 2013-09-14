1e0f35334d07dce4fa72cbc910df0aa18393ad24
bear <aluba0504@gmail.com>
2013-09-14 13:03:46 +0000

    把年份、月份改成變數

--- src/org/bear/main/BuildRevenueSFI.java
+++ src/org/bear/main/BuildRevenueSFI.java
@@ -15,26 +15,25 @@ public class BuildRevenueSFI {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String year = "2013";
+		String preMonth = "7";
+		String month = "8";
 		GetSFIContent getContent;
 		ImportPriceSFI sfi = new ImportPriceSFI();
 		/* 上市公司價格資訊 */
-		//getContent = new GetSFIPrice();
-		//sfi.insertBatchList("2011", "12", "2013", "7", getContent);
+		getContent = new GetSFIPrice();
+		sfi.insertBatchList(year, preMonth, year, month, getContent);
 		/* 證交所平均價與週轉率 */
-		//getContent = new GetTwseIndividualIndex();
-		//sfi.insertBatchList("2010", null, null, null, getContent);
-		//sfi.insertBatchList("2011", null, null, null, getContent);
-		//sfi.insertBatchList("2012", null, null, null, getContent);
-		//sfi.insertBatchList("2013", null, null, null, getContent);
+		getContent = new GetTwseIndividualIndex();
+		sfi.insertBatchList(year, null, null, null, getContent);
 		/* 櫃臺月成交資訊與週轉率 */
-		//getContent = new GretaiIndividualIndex(); 
-		//sfi.insertBatchList("2012", null, null, null, getContent);		
-		//sfi.insertBatchList("2013", null, null, null, getContent);
+		getContent = new GretaiIndividualIndex(); 		
+		sfi.insertBatchList(year, null, null, null, getContent);
 		/* 上市營收資訊 */
 		getContent = new GetSFITwseRevenue();
-		sfi.insertBatchList("2012", "1", "2013", "7", getContent);
+		sfi.insertBatchList(year, month, year, month, getContent);
 		/* 上櫃營收資訊 */
 		getContent = new GetSFIGrateiRevenue();
-		sfi.insertBatchList("2012", "1", "2013", "7", getContent);
+		sfi.insertBatchList(year, month, year, month, getContent);
 	}
 }
