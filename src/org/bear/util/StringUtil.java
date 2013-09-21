76819fd8aa36dd283c5642f8d374ba0cb0b8d7a8
bear <aluba0504@gmail.com>
2013-09-21 12:27:47 +0000

    Double精確減法Utility

--- src/org/bear/util/StringUtil.java
+++ src/org/bear/util/StringUtil.java
@@ -1,5 +1,6 @@
 package org.bear.util;
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
@@ -55,5 +56,11 @@ public class StringUtil
 		NumberFormat formatter = new DecimalFormat(decimal);
 		number = Double.parseDouble(formatter.format(number));
 		return number;
+	}
+	public static double sub(double v1, double v2) 
+	{
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.subtract(b2).doubleValue();
 	}
 }
