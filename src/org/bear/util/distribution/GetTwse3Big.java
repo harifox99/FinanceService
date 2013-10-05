d5f31a0a9043ae445fb996c5752cd2c0593a132d
bear <aluba0504@gmail.com>
2013-10-05 11:09:48 +0000

    擷取證交所三大法人的URL

---
+++ src/org/bear/util/distribution/GetTwse3Big.java
@@ -0,0 +1,44 @@
+package org.bear.util.distribution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.bear.dao.JdbcThreeBigDao;
+import org.bear.parser.distribution.Twse3BigParser;
+import org.bear.util.HttpUtil;
+
+public class GetTwse3Big 
+{
+	JdbcThreeBigDao threeBigDao;
+	
+	public JdbcThreeBigDao getThreeBigDao() {
+		return threeBigDao;
+	}
+
+	public void setThreeBigDao(JdbcThreeBigDao threeBigDao) {
+		this.threeBigDao = threeBigDao;
+	}
+
+	public void getContent(String stockID, String startYear, String startMonth, String endYear, String endMonth)
+	{
+		Twse3BigParser parser = new Twse3BigParser();
+		parser.setThreeBigDao(threeBigDao);
+		parser.setDateString(startYear + startMonth);
+		String url = "http://www.twse.com.tw/ch/trading/fund/TWT47U/TWT47U.php";
+		List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+		paramList.add(new BasicNameValuePair("yr", startYear));
+		paramList.add(new BasicNameValuePair("w_date", startYear + startMonth + "01"));
+		//證券種類若為個位數補0
+		if (stockID.length() <= 1)
+			paramList.add(new BasicNameValuePair("select2", "0" + stockID));
+		else
+			paramList.add(new BasicNameValuePair("select2", stockID));
+		paramList.add(new BasicNameValuePair("value", "查詢"));
+		String responseString = HttpUtil.send(url, paramList, 1, "big5");
+		//System.out.println(responseString);
+		parser.setResponseString(responseString);
+		parser.parse(7);	
+	}
+}
