82e85b2e713be077437cba623d6f6681878d68c9
bear <aluba0504@gmail.com>
2013-10-05 10:47:40 +0000

    Erase unused character

--- src/org/bear/dao/MacroEconomicDao.java
+++ src/org/bear/dao/MacroEconomicDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Date;
 public interface MacroEconomicDao 
 {
-	public void insertBatch(List <MacroEconomicEntity>  entity);
+	public void insertBatch(List <MacroEconomicEntity> entity);
 	public List <MacroEconomicEntity> findAll();
 	public List <MacroEconomicEntity> findByDate(Date startTime, Date endTime);
 	public int update(String indexName, String indexValue, String date, String split);
