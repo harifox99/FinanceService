5893d311388cb88236cdb84564bde78812a661b1
bear <aluba0504@gmail.com>
2013-10-06 11:14:23 +0000

    可更新董監持股數量

--- src/org/bear/dao/ThreeBigDao.java
+++ src/org/bear/dao/ThreeBigDao.java
@@ -6,4 +6,5 @@ import org.bear.entity.ThreeBigEntity;
 public interface ThreeBigDao {
 	public void insert(ThreeBigEntity entity); 
 	public List<ThreeBigEntity> latest(int duration);
+	public int update(String indexName, String indexValue, String date, String stockID);
 }
