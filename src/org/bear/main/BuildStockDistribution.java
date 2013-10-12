4d462a9e33182499e6833765581d3006f1cddea2
bear <aluba0504@gmail.com>
2013-10-12 11:47:43 +0000

    移除擷取董監持股的部分程式（因為每個月20號更新資料）

--- src/org/bear/main/BuildStockDistribution.java
+++ src/org/bear/main/BuildStockDistribution.java
@@ -5,12 +5,9 @@ import org.bear.dao.JdbcStockTypeDao;
 import org.bear.dao.JdbcThreeBigDao;
 import org.bear.datainput.ImportStockIDData;
 import org.bear.parser.distribution.Gretai3BigParser;
-import org.bear.parser.distribution.StockHolderParser;
-import org.bear.util.StringUtil;
 import org.bear.util.distribution.GetGretai3Big;
 import org.bear.util.distribution.StockDistribution;
 import org.bear.util.distribution.GetTwse3Big;
-import org.bear.util.distribution.StockHolderDistribution;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -78,21 +75,6 @@ public class BuildStockDistribution extends ImportStockIDData
 			parser.parse(2);
 		}
 		
-		//董監持股改用證交所資料, http://siis.twse.com.tw/publish/sii
-		//SupervisorDistribution supervisor = new SupervisorDistribution();
-		//SupervisorParser parser = new SupervisorParser();
-		//parser.setElementList(supervisor.getContent());
-		//parser.setThreeBigDao(threeBigDao);
-		//parser.setDateString(startYear + monthList[0] + "01");
-		//parser.parse(5);
-		for (int i = 0; i < monthList.length; i++)
-		{
-			StockHolderDistribution stockHolder = new StockHolderDistribution(StringUtil.convertChineseYear(startYear), monthList[i]);
-			StockHolderParser stockHolderParser = new StockHolderParser();
-			stockHolderParser.setThreeBigDao(threeBigDao);
-			stockHolderParser.setElementList(stockHolder.getContent());
-			stockHolderParser.setDateString(startYear + "-" + monthList[i] + "-01");
-			stockHolderParser.parse(1);
-		}
+		
 	}
 }
