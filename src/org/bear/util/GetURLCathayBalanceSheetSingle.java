df5adbcbc2e8f4e287418405a58d3bae828cc958
bear <aluba0504@gmail.com>
2013-11-16 12:59:04 +0000

    新增玉山證券「資產負債表」網址

--- src/org/bear/util/GetURLCathayBalanceSheetSingle.java
+++ src/org/bear/util/GetURLCathayBalanceSheetSingle.java
@@ -4,6 +4,7 @@ public class GetURLCathayBalanceSheetSingle extends GetURLContentBase {
 
 	public GetURLCathayBalanceSheetSingle(String stockID, boolean isYear) {
 		urlHeaderSeason = "http://dj.mybank.com.tw/z/zc/zcp/zcpa/zcpa0_";
+		//urlHeaderSeason = "http://www.esunsec.com.tw/z/zc/zcp/zcpa/zcpa0_";
 		urlHeaderYear = "http://dj.mybank.com.tw/z/zc/zcp/zcpb/zcpb0_";
 		urlFooter = ".asp.htm";
 		if (isYear)
