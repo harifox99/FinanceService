d8dfe9435849317c2e0d013375f4d7c14f872a4f
bear <aluba0504@gmail.com>
2013-12-14 07:17:17 +0000

    更新總經資料查詢參數

--- src/org/bear/main/BuildMacroEconomicData.java
+++ src/org/bear/main/BuildMacroEconomicData.java
@@ -26,9 +26,9 @@ public class BuildMacroEconomicData extends ParseFile
 	{
 		// TODO Auto-generated method stub
 		String startYear = "2013";
-		String startMonth = "9";
+		String startMonth = "10";
 		String endYear = "2013";
-		String endMonth = "9";
+		String endMonth = "10";
 		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 		JdbcMacroEconomicDao dao = (JdbcMacroEconomicDao)context.getBean("macroEconomicDao");
 		//CEPD
@@ -46,12 +46,12 @@ public class BuildMacroEconomicData extends ParseFile
 		//³f¹ô
 		GetCbcMoney money = new GetCbcMoney();
 		money.setDao(dao);
-		money.getContent(CbcIndexConstant.MONTH_HASH.get("2013M04"), CbcIndexConstant.MONTH_HASH.get("2013M09"));
+		money.getContent(CbcIndexConstant.MONTH_HASH.get("2013M04"), CbcIndexConstant.MONTH_HASH.get("2013M10"));
 		
 		//¬¡´ÁÀx»W¦s´Ú
 		GetDemandDeposit deposit = new GetDemandDeposit();
 		deposit.setDao(dao);
-		deposit.getContent(CbcIndexConstant.MONTH_HASH.get("2013M04"), CbcIndexConstant.MONTH_HASH.get("2013M09"));
+		deposit.getContent(CbcIndexConstant.MONTH_HASH.get("2013M04"), CbcIndexConstant.MONTH_HASH.get("2013M10"));
 		
 		//TWSE
 		TwseIndex twseIndex = new TwseIndex();
