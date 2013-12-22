bb7350e8fabadeba7bcd6b177101889bb4b046f6
bear <aluba0504@gmail.com>
2013-12-22 06:43:28 +0000

    新增VIX指數

--- src/org/bear/main/BuildAmericanMacroData.java
+++ src/org/bear/main/BuildAmericanMacroData.java
@@ -20,11 +20,13 @@ public class BuildAmericanMacroData extends ParseFile
 	 * @param args
 	 */
 	List <AmericanMacroPantherEntity> list;
+	
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		//new BuildAmericanMacroData().insertBatch();
 		//new BuildMacroEconomicData().findAllList();
+		//記得改FredEconomicUrl的observation_init
 		FredEconomicUrl type = new FredEconomicUrl();
 		new BuildAmericanMacroData().insertByFredApi(FredIndexParameterMap.AMERICA_LIST, type, FredIndexParameterMap.HOUST);
 		new BuildAmericanMacroData().insertByFredApi(FredIndexParameterMap.AMERICA_PAYEMS_LIST, type, FredIndexParameterMap.PAYEMS);
@@ -32,6 +34,7 @@ public class BuildAmericanMacroData extends ParseFile
 		new BuildAmericanMacroData().insertByFredApi(FredIndexParameterMap.AMERICA_INTEREST_LIST, type, FredIndexParameterMap.INTEREST);
 		new BuildAmericanMacroData().insertByFredApi(FredIndexParameterMap.AMERICA_YOY_LIST, type, FredIndexParameterMap.CPIAUCSL);
 		new BuildAmericanMacroData().insertByFredApi(FredIndexParameterMap.AMERICA_MONEY_LIST, type, FredIndexParameterMap.MONEY);
+		new BuildAmericanMacroData().insertByFredApi(FredIndexParameterMap.AMERICA_VIX_LIST, type, FredIndexParameterMap.VIX);
 	}
 	public void insertByFredApi(String[] catagoryList, FredEconomicUrl catagory, HashMap<String, String> parameterMap)
 	{
@@ -71,14 +74,16 @@ public class BuildAmericanMacroData extends ParseFile
 					}
 					dao.insertBatch(list);
 				}
+				else if (catagoryList[i].equals("VIXCLS"))
+					this.saveVixData(map, dao);
 				//之後用日期當Key，更新其他欄位資料
 				else
-				{
+				{					
 					for (Map.Entry<String,String> entry : map.entrySet()) 
 					{				
 						int result;						
 						if (catagoryList[i].equals("CPIAUCSL"))
-							result = dao.update("CPI", entry.getValue(), entry.getKey());
+							result = dao.update("CPI", entry.getValue(), entry.getKey());						
 						else
 							result = dao.update(catagoryList[i], entry.getValue(), entry.getKey());
 					    if (result <= 0)
@@ -119,5 +124,38 @@ public class BuildAmericanMacroData extends ParseFile
 		list = dao.findByDate(startTime, endTime);		
 		return list;
 	}
-
+	private void saveVixData(HashMap<String, String> map, AmericanMacroPantherDao dao)
+	{
+		HashMap<String, String> vixHigh = new HashMap<String, String>();
+		HashMap<String, String> vixLow = new HashMap<String, String>();
+		for (Map.Entry<String,String> entry : map.entrySet()) 
+		{
+			String key = entry.getKey().substring(0, 7);
+			String data = entry.getValue();
+			if (vixHigh.get(key) == null)
+			{
+				vixHigh.put(key, data);
+			}
+			else if (Double.parseDouble(data) > Double.parseDouble(vixHigh.get(key)))
+			{
+				vixHigh.put(key, data);
+			}
+			if (vixLow.get(key) == null)
+			{
+				vixLow.put(key, data);
+			}
+			else if (Double.parseDouble(data) < Double.parseDouble(vixLow.get(key)))
+			{							
+				vixLow.put(key, data);
+			}
+		}
+		for (Map.Entry<String,String> entry : vixHigh.entrySet()) 
+		{
+			dao.update("VIXHigh", entry.getValue(), entry.getKey() + "-01");	
+		}
+		for (Map.Entry<String,String> entry : vixLow.entrySet()) 
+		{
+			dao.update("vixLow", entry.getValue(), entry.getKey() + "-01");	
+		}
+	}
 }
