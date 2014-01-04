11a8ff07250a93c98ac9e538cd8417f4aa14f80b
bear <aluba0504@gmail.com>
2014-01-04 13:07:58 +0000

    修改櫃臺三大法人買賣超網址

--- src/org/bear/util/distribution/GetGretai3Big.java
+++ src/org/bear/util/distribution/GetGretai3Big.java
@@ -7,8 +7,10 @@ public class GetGretai3Big extends GetURLContentBase
 {
 	public GetGretai3Big(String year, String month)
 	{
-		urlHeader = "http://www.otc.org.tw/ch/stock/3insti/DAILY_TradE/BIGM_";
-		urlFooter = ".html";
-		urlString = urlHeader + StringUtil.convertChineseYear(year) + month + urlFooter;		
+		//urlHeader = "http://www.otc.org.tw/ch/stock/3insti/DAILY_TradE/BIGM_";
+		urlHeader = "http://www.otc.org.tw/ch/stock/3insti/DAILY_TradE/3itrade_print.php?t=M&d=";
+		urlFooter = "&s=0,asc,0";
+		urlString = urlHeader + StringUtil.convertChineseYear(year) + "/" + month + urlFooter;
+		System.out.println(urlString);
 	}
 }
