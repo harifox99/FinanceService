ffcf1eefd5e5eed981e0fbe765dea7cc8a2be2bd
bear <aluba0504@gmail.com>
2014-04-13 12:56:55 +0000

    部分資產負債表的資料從玉山證券而不是國泰世華證券

--- src/org/bear/util/GetURLCathayBalanceSheetSingle.java
+++ src/org/bear/util/GetURLCathayBalanceSheetSingle.java
@@ -3,10 +3,11 @@ package org.bear.util;
 public class GetURLCathayBalanceSheetSingle extends GetURLContentBase {
 
 	public GetURLCathayBalanceSheetSingle(String stockID, boolean isYear) {
-		urlHeaderSeason = "http://dj.mybank.com.tw/z/zc/zcp/zcpa/zcpa0_";
-		//urlHeaderSeason = "http://www.esunsec.com.tw/z/zc/zcp/zcpa/zcpa0_";
-		urlHeaderYear = "http://dj.mybank.com.tw/z/zc/zcp/zcpb/zcpb0_";
-		urlFooter = ".asp.htm";
+		//urlHeaderSeason = "http://dj.mybank.com.tw/z/zc/zcp/zcpa/zcpa0_";
+		urlHeaderSeason = "http://www.esunsec.com.tw/z/zc/zcp/zcpa/zcpa0_";
+		//urlHeaderYear = "http://dj.mybank.com.tw/z/zc/zcp/zcpb/zcpb0_";
+		urlHeaderYear = "http://www.esunsec.com.tw/z/zc/zcp/zcpb/zcpb0_";
+		urlFooter = ".djhtm";
 		if (isYear)
 			this.urlString = urlHeaderYear + stockID + urlFooter;
 		else
