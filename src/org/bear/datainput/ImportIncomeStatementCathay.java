1274988fc9d610413d568d9629e09e00dabcb497
bear <aluba0504@gmail.com>
2014-05-19 11:39:59 +0000

    修改日期參數

--- src/org/bear/datainput/ImportIncomeStatementCathay.java
+++ src/org/bear/datainput/ImportIncomeStatementCathay.java
@@ -3,6 +3,7 @@
  */
 package org.bear.datainput;
 
+import org.bear.constant.FinancialReport;
 import org.bear.parser.IncomeStatementParserCathay;
 import org.bear.util.GetURLCathayIncomeStatement;
 
@@ -17,26 +18,26 @@ public class ImportIncomeStatementCathay extends ImportStockIDData
 		try
 		{							
 			int idleTime = 0;
+			
 			for (int j = 0; j < wrapperList.size(); j++)
 			{
-				int expectedNum = 7;
-				String[] seasons = {"01", "02", "03", "04"};
-				String[] years = {"2012", "2013"};
+				int expectedNum = FinancialReport.expectedNum;
+				String[] seasons = {"01"};
+				String[] years = {"2014"};
 				String stockID = wrapperList.get(j).getStockID();
 				System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");
 				//季資料
 				GetURLCathayIncomeStatement urlContent = new GetURLCathayIncomeStatement(stockID, false);
 				IncomeStatementParserCathay incomeStatementYear = new IncomeStatementParserCathay(urlContent.getContent(), stockID, false, years, seasons, expectedNum, true);
 				incomeStatementYear.parse(2);
-				Thread.sleep(2000);	
+				Thread.sleep(3000);	
 				idleTime++;
 			}
-			//for (int j = 0; j < wrapperList.size(); j++)
 			/*
-			for (int j = 585; j < wrapperList.size(); j++)
+			for (int j = 1102; j < wrapperList.size(); j++)
 			{
-				int expectedNum = 8;
-				String[] years = {"2005","2006","2007","2008","2009","2010","2011","2012"};
+				int expectedNum = 1;
+				String[] years = {"2013"};
 				String[] seasons = {"00"};
 				String stockID = wrapperList.get(j).getStockID();
 				System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");
@@ -44,7 +45,7 @@ public class ImportIncomeStatementCathay extends ImportStockIDData
 				GetURLCathayIncomeStatement urlContent = new GetURLCathayIncomeStatement(stockID, true);
 				IncomeStatementParserCathay incomeStatementYear = new IncomeStatementParserCathay(urlContent.getContent(), stockID, true, years, seasons, expectedNum, true);
 				incomeStatementYear.parse(2);
-				Thread.sleep(5000);	
+				Thread.sleep(3000);	
 				idleTime++;
 			}*/
 		}
