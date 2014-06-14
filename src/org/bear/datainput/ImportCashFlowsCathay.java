604040dfda95e655417ae6cdf226365dd3f2cd39
bear <aluba0504@gmail.com>
2014-06-14 16:07:38 +0000

    Catch NullPointerException後(通常是Connection Timeout)，可以讓程式重新連線，再次擷取資料

--- src/org/bear/datainput/ImportCashFlowsCathay.java
+++ src/org/bear/datainput/ImportCashFlowsCathay.java
@@ -1,4 +1,5 @@
 package org.bear.datainput;
+import org.bear.constant.FinancialReport;
 import org.bear.parser.CashFlowsParserCathay;
 import org.bear.util.*;
 /**
@@ -12,34 +13,45 @@ public class ImportCashFlowsCathay extends ImportStockIDData
 		try
 		{			
 			int idleTime = 0;
-			//年資料
+			//年資料	
 			/*
-			for (int i = 1383; i < wrapperList.size(); i++)
+			for (int i = 1361; i < wrapperList.size(); i++)
 			{
-				int expectedNum = 8;
-				String[] years = {"2005","2006","2007","2008","2009","2010","2011","2012"};
+				int expectedNum = 1;
+				String[] years = {"2013"};
 				String[] seasons = {"00"};
 				String stockID = wrapperList.get(i).getStockID();
 				System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");
 				GetURLCathayCashFlow urlContent = new GetURLCathayCashFlow(stockID, true);
 				CashFlowsParserCathay cashFlowsParser = new CashFlowsParserCathay(urlContent.getContent(), stockID, years, seasons, true, expectedNum, true);
 				cashFlowsParser.parse(2);
-				Thread.sleep(5000);		
+				Thread.sleep(3000);		
 				idleTime++;
 			}*/
 			
 			//季資料
-			for (int i = 0; i < wrapperList.size(); i++)
+			for (int i = 1391; i < wrapperList.size(); i++)
 			{
-				int expectedNum = 7;
-				String[] seasons = {"01", "02", "03", "04"};
-				String[] years = {"2012", "2013"};
+				int expectedNum = FinancialReport.expectedNum;
+				String[] seasons = {"01"};
+				String[] years = {"2014"};
 				String stockID = wrapperList.get(i).getStockID();
+				//if (!stockID.equals("2850"))
+					//continue;
 				System.out.println("股票代碼：" + stockID + " " + idleTime + ". ");
 				GetURLCathayCashFlow urlContent = new GetURLCathayCashFlow(stockID, false);
 				CashFlowsParserCathay cashFlowsParser = new CashFlowsParserCathay(urlContent.getContent(), stockID, years, seasons, false, expectedNum, true);
-				cashFlowsParser.parse(2);
-				Thread.sleep(2000);		
+				try
+				{					
+					cashFlowsParser.parse(2);
+				}
+				catch (NullPointerException ex)
+				{
+					System.out.println("NullPointerException!");
+					idleTime--;
+					i--;
+				}
+				Thread.sleep(3000);		
 				idleTime++;
 			}
 		}
