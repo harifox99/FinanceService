c0b9f917be82571dd240b6903cfdf095f454aeae
bear <aluba0504@gmail.com>
2014-06-14 16:00:31 +0000

    修改URL（沒辦法，國泰的有些有問題）

--- src/org/bear/util/GetURLCathayBasicData.java
+++ src/org/bear/util/GetURLCathayBasicData.java
@@ -12,8 +12,10 @@ public class GetURLCathayBasicData extends GetURLContentBase
 	public GetURLCathayBasicData(String stockID)
 	{
 		urlHeaderSeason = "";
-		urlHeaderYear = "http://dj.mybank.com.tw/z/zc/zca/zca_";
-		urlFooter = ".asp.htm";
+		//urlHeaderYear = "http://dj.mybank.com.tw/z/zc/zca/zca_";
+		//urlFooter = ".asp.htm";
+		urlHeaderYear = "http://www.esunsec.com.tw/z/zc/zca/zca.djhtm?A=";		
+		urlFooter = "";
 		this.urlString = urlHeaderYear + stockID + urlFooter;
 		System.out.println(this.urlString);
 	}
