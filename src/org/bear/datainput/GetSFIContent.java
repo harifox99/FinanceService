7d7ff191b83c77d2432af13de55d7aeea627f8e4
bear <aluba0504@gmail.com>
2015-06-06 16:07:26 +0000

    GetSFIContent的getContent()多+1個參數，叫做是stockName

--- src/org/bear/datainput/GetSFIContent.java
+++ src/org/bear/datainput/GetSFIContent.java
@@ -1,5 +1,5 @@
 package org.bear.datainput;
 
 public interface GetSFIContent {
-	public void getContent(String stockID, String startYear, String startMonth, String endYear, String endMonth);
+	public void getContent(String stockID, String stockName, String startYear, String startMonth, String endYear, String endMonth);
 }
