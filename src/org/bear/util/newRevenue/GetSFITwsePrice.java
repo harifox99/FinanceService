347b1b6b70d29117041b9d23c7e43e79a366c0c1
bear <aluba0504@gmail.com>
2015-06-06 15:57:22 +0000

    Refactor and add comments

---
+++ src/org/bear/util/newRevenue/GetSFITwsePrice.java
@@ -0,0 +1,41 @@
+package org.bear.util.newRevenue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.bear.datainput.GetSFIContent;
+import org.bear.parser.sfi.PriceParser;
+import org.bear.util.HttpUtil;
+/**
+ * 上市公司成交價格資訊
+ * @author edward
+ *
+ */
+public class GetSFITwsePrice implements GetSFIContent 
+{
+	public void getContent(String stockID, String stockName, String startYear, String startMonth,
+            String endYear, String endMonth)
+	{
+    	PriceParser parser = new PriceParser();
+		String url = "http://webline.sfi.org.tw/B/intdb/single/sfis507b.asp";
+		List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+		paramList.add(new BasicNameValuePair("SCODE", stockID));
+		paramList.add(new BasicNameValuePair("SYY", startYear));
+		paramList.add(new BasicNameValuePair("SMM", startMonth));
+		paramList.add(new BasicNameValuePair("EYY", endYear));
+		paramList.add(new BasicNameValuePair("EMM", endMonth));
+		String responseString = HttpUtil.send(url, paramList, 1, "big5");
+		parser.setResponseString(responseString);
+		parser.setStockID(stockID);
+		parser.parse(2);
+		//System.out.println(responseString);		
+	}
+    public static void main(String args[])
+    {
+    	//GetSFIPrice revenue = new GetSFIPrice();
+    	GetSFITwseRevenue revenue = new GetSFITwseRevenue();
+    	revenue.getContent("1101", null, "2013", "3", "2013", "6");
+    }
+}
