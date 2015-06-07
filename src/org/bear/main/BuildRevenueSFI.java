6b2d932fcd66a215e5d08e1f82cb4b3de01125cd
bear <aluba0504@gmail.com>
2015-06-07 00:45:08 +0000

    修改日期，並新增到公開資訊觀測站擷取營收資料的程式

--- src/org/bear/main/BuildRevenueSFI.java
+++ src/org/bear/main/BuildRevenueSFI.java
@@ -1,8 +1,7 @@
 package org.bear.main;
 import org.bear.datainput.GetSFIContent;
 import org.bear.datainput.ImportPriceSFI;
-import org.bear.util.newRevenue.GetSFIGrateiRevenue;
-import org.bear.util.newRevenue.GetSFITwseRevenue;
+import org.bear.util.newRevenue.*;
 
 public class BuildRevenueSFI {
 
@@ -12,16 +11,19 @@ public class BuildRevenueSFI {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String startYear = "2014";
-		String startMonth = "9";
+		String startYear = "2015";
+		String startMonth = "5";
 		String endYear = "2015";
-		String endMonth = "4";
+		String endMonth = "5";
 		GetSFIContent getContent;
 		ImportPriceSFI sfi = new ImportPriceSFI();
-		/* 上市營收資訊 */
+		/* 上市/上櫃營收資訊 (公開資訊觀測站) */
+		//getContent = new GetMopsRevenue();
+		//sfi.insertBatchList(startYear, StringUtil.addZeroMonth(startMonth), endYear, endMonth, getContent);
+		/* 上市營收資訊 (證券期貨發展基金會) */
 		getContent = new GetSFITwseRevenue();
 		sfi.insertBatchList(startYear, startMonth, endYear, endMonth, getContent);
-		/* 上櫃營收資訊 */
+		/* 上櫃營收資訊 (證券期貨發展基金會) */
 		getContent = new GetSFIGrateiRevenue();
 		sfi.insertBatchList(startYear, startMonth, endYear, endMonth, getContent);
 	}
