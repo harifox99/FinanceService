d3245a3ae14346da5cc4c66ae5fd497bcc09c947
bear <aluba0504@gmail.com>
2015-06-07 03:51:39 +0000

    修改日期

--- src/org/bear/main/BuildTaiwanPrice.java
+++ src/org/bear/main/BuildTaiwanPrice.java
@@ -2,7 +2,7 @@ package org.bear.main;
 
 import org.bear.datainput.GetSFIContent;
 import org.bear.datainput.ImportPriceSFI;
-import org.bear.util.newRevenue.GetSFIPrice;
+import org.bear.util.newRevenue.GetSFITwsePrice;
 import org.bear.util.newRevenue.GetTwseIndividualIndex;
 import org.bear.util.newRevenue.GretaiIndividualIndex;
 /**
@@ -14,20 +14,20 @@ public class BuildTaiwanPrice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String preYear = "2014";
-		String preMonth = "7";
-		String year = "2014";
-		String month = "8";
+		String preYear = "2015";
+		String preMonth = "4";
+		String year = "2015";
+		String month = "5";
 		GetSFIContent getContent;
 		ImportPriceSFI sfi = new ImportPriceSFI();
 		/* 上市公司價格資訊 */
-		getContent = new GetSFIPrice();
+		getContent = new GetSFITwsePrice();
 		sfi.insertBatchList(preYear, preMonth, year, month, getContent);
-		/* 證交所平均價與週轉率 */
+		/* 證交所平均價與週轉率, sleep time可以調成1000ms */
 		getContent = new GetTwseIndividualIndex();
 		sfi.insertBatchList(year, null, null, null, getContent);
 		/* 櫃臺月成交資訊與週轉率 */
 		getContent = new GretaiIndividualIndex(); 		
-		sfi.insertBatchList(year, month, null, null, getContent);
+		sfi.insertBatchList(year, preMonth, null, month, getContent);
 	}
 }
