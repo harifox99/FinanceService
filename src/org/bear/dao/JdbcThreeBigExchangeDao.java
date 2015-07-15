9ca2142d157244f8f556f983b7e173e222d0d666
bear <aluba0504@gmail.com>
2015-07-15 08:15:35 +0000

    三大法人買賣超Entity

---
+++ src/org/bear/dao/JdbcThreeBigExchangeDao.java
@@ -0,0 +1,42 @@
+package org.bear.dao;
+
+import java.util.List;
+import org.bear.entity.ThreeBigExchangeEntity;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
+
+public class JdbcThreeBigExchangeDao extends SimpleJdbcDaoSupport implements ThreeBigExchangeDao{
+
+	@Override
+	public void insert(ThreeBigExchangeEntity entity) {
+		try
+		{
+			String sql = "insert into ThreeBigExchange" + "" +
+			"(StockID, ExchangeDate, Quantity, StockBranch, Rank, Exchanger) values " +
+			"(:stockID, :exchangeDate, :quantity, :stockBranch, :rank, :exchanger)";
+			//System.out.println("sql: " + sql);
+			SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(entity);
+			this.getSimpleJdbcTemplate().update(sql, parameterSource);
+		}
+		catch (Exception ex)
+		{
+			//ex.printStackTrace();
+			System.out.println("Insert Error!");
+		}
+
+	}
+
+	@Override
+	public List<ThreeBigExchangeEntity> latest(String stockID, int duration) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ThreeBigExchangeEntity> latest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/org/bear/dao/ThreeBigExchangeDao.java
@@ -0,0 +1,9 @@
+package org.bear.dao;
+import java.util.List;
+import org.bear.entity.ThreeBigExchangeEntity;
+
+public interface ThreeBigExchangeDao {
+	public void insert(ThreeBigExchangeEntity entity); 
+	public List<ThreeBigExchangeEntity> latest(String stockID, int duration);
+	public List<ThreeBigExchangeEntity> latest();
+}
