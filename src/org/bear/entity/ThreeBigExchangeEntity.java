9538ab52a6c4c0e8c620700ef3b38c4f88a86642
bear <aluba0504@gmail.com>
2015-07-15 08:17:21 +0000

    三大法人買賣超Entity

---
+++ src/org/bear/entity/ThreeBigExchangeEntity.java
@@ -0,0 +1,49 @@
+package org.bear.entity;
+
+import java.util.Date;
+
+public class ThreeBigExchangeEntity {
+	Date exchangeDate;
+	String stockID;
+	int rank;
+	int stockBranch;
+	int quantity;
+	String exchanger;
+	public Date getExchangeDate() {
+		return exchangeDate;
+	}
+	public void setExchangeDate(Date exchangeDate) {
+		this.exchangeDate = exchangeDate;
+	}
+	public String getStockID() {
+		return stockID;
+	}
+	public void setStockID(String stockID) {
+		this.stockID = stockID;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	public int getStockBranch() {
+		return stockBranch;
+	}
+	public void setStockBranch(int stockBranch) {
+		this.stockBranch = stockBranch;
+	}
+	public String getExchanger() {
+		return exchanger;
+	}
+	public void setExchanger(String exchanger) {
+		this.exchanger = exchanger;
+	}
+	
+}
