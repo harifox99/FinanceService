b62cece7acb2efcba927c8b203ebf4d3b57e6575
bear <aluba0504@gmail.com>
2015-07-15 08:14:45 +0000

    投信外資買賣超前N名

--- src/org/bear/constant/FinancialReport.java
+++ src/org/bear/constant/FinancialReport.java
@@ -3,4 +3,6 @@ package org.bear.constant;
 public class FinancialReport {
 	public static int expectedNum = 4;
 	public static int sleepTime = 1000;
+	//投信外資買賣超前N(30)名
+	public static int maxNumber = 30;
 }
