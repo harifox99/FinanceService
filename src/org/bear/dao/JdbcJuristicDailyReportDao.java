3c8f551c5f240cd5f5b2836675cec408f15f036a
bear <aluba0504@gmail.com>
2015-07-18 14:36:05 +0000

    擷取個股法人籌碼資訊的DAO

--- src/org/bear/dao/JdbcJuristicDailyReportDao.java
+++ src/org/bear/dao/JdbcJuristicDailyReportDao.java
@@ -1,6 +1,7 @@
 package org.bear.dao;
 import java.util.List;
 import org.bear.entity.JuristicDailyEntity;
+import org.bear.entity.ThreeBigExchangeEntity;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.ParameterizedBeanPropertyRowMapper;
@@ -43,7 +44,13 @@ public class JdbcJuristicDailyReportDao extends SimpleJdbcDaoSupport implements 
 		entityList = this.getSimpleJdbcTemplate().query(sql, ParameterizedBeanPropertyRowMapper.newInstance(JuristicDailyEntity.class));
 		return entityList;
 	}
-	
-	
 
+	@Override
+	public List<ThreeBigExchangeEntity> findSingleStock(String stockID, int size) {
+		// TODO Auto-generated method stub
+		List <ThreeBigExchangeEntity> entityList = null;
+		String sql = "select top " + size + " * from ThreeBigExchange where stockID = ? ORDER BY ExchangeDate desc";
+		entityList = this.getSimpleJdbcTemplate().query(sql, ParameterizedBeanPropertyRowMapper.newInstance(ThreeBigExchangeEntity.class), stockID);
+		return entityList;
+	}
 }
--- src/org/bear/dao/JuristicDailyReportDao.java
+++ src/org/bear/dao/JuristicDailyReportDao.java
@@ -2,6 +2,7 @@ package org.bear.dao;
 
 import java.util.List;
 import org.bear.entity.JuristicDailyEntity;
+import org.bear.entity.ThreeBigExchangeEntity;
 
 /**
  * 三大法人交易日報DAO
@@ -12,4 +13,5 @@ public interface JuristicDailyReportDao {
 	public void insert(JuristicDailyEntity entity); 
 	public int update(String indexName, int indexValue, String date); 
 	public List<JuristicDailyEntity> findLatestData(int size);
+	public List<ThreeBigExchangeEntity> findSingleStock(String stockID, int size);
 }
