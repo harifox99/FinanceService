3397a4bf24b65e4f3b519f40a39abce48ab84587
bear <aluba0504@gmail.com>
2015-09-01 07:31:05 +0000

    修改Http Post參數

--- src/org/bear/util/cbc/GetStockValue.java
+++ src/org/bear/util/cbc/GetStockValue.java
@@ -10,7 +10,8 @@ import org.bear.util.HttpUtil;
 public class GetStockValue extends GetStatDbData {
 	public void getContent(int startDateValue, int endDateValue)
 	{		
-		url = "http://statdb.dgbas.gov.tw/pxweb/Dialog/Saveshow.asp";
+		//url = "http://statdb.dgbas.gov.tw/pxweb/Dialog/Saveshow.asp";
+		url = "http://www.pxweb.cbc.gov.tw/Dialog/Saveshow.asp";
 		//初始值
 		paramList.add(new BasicNameValuePair("Valdavarden1", String.valueOf(endDateValue-startDateValue+1)));
 		paramList.add(new BasicNameValuePair("Valdavarden2", "1"));
@@ -25,13 +26,14 @@ public class GetStockValue extends GetStatDbData {
 		//原始值
 		paramList.add(new BasicNameValuePair("values3", "1"));
 		/************************************/
-		paramList.add(new BasicNameValuePair("matrix", "FM2902A1M"));
+		//paramList.add(new BasicNameValuePair("matrix", "FM2902A1M"));
+		paramList.add(new BasicNameValuePair("matrix", "EG27M01"));
 		//paramList.add(new BasicNameValuePair("varparm", "ma=ES0101A1M&amp;ti=%B4%BA%AE%F0%BB%E2%A5%FD%AB%FC%BC%D0%2D%A4%EB&amp;path=%2E%2E%2FPXfile%2FEconomicStatistics%2F&amp;xu=&amp;yp=&amp;lang=9"));
 		paramList.add(new BasicNameValuePair("hasAggregno", "0"));	
 		//paramList.add(new BasicNameValuePair("ti", "景氣領先指標-月"));
 		paramList.add(new BasicNameValuePair("pxkonv", "asp1"));		
-		paramList.add(new BasicNameValuePair("root", "../PXfile/FinancialStatistics/"));
-		paramList.add(new BasicNameValuePair("classdir", "../PXfile/FinancialStatistics/"));
+		paramList.add(new BasicNameValuePair("root", "../PXfile/EFS/"));
+		paramList.add(new BasicNameValuePair("classdir", "../PXfile/EFS/"));
 		paramList.add(new BasicNameValuePair("noofvar", "3"));
 		paramList.add(new BasicNameValuePair("elim", "NNN"));
 		//paramList.add(new BasicNameValuePair("numberstub", "1"));
