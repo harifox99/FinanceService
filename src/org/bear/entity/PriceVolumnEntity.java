303e5cb511769e4a3a06af7bbb0fdd2f3b50bcef
bear <aluba0504@gmail.com>
2015-10-03 12:18:49 +0000

    價量分析Entity

---
+++ src/org/bear/entity/PriceVolumnEntity.java
@@ -0,0 +1,36 @@
+package org.bear.entity;
+/**
+ * »ù¶q¤ÀªR
+ * @author edward
+ *
+ */
+public class PriceVolumnEntity {
+	int volumn;
+	double price;
+	int averageVolumn;
+	double averagePrice;
+	public int getVolumn() {
+		return volumn;
+	}
+	public void setVolumn(int volumn) {
+		this.volumn = volumn;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getAverageVolumn() {
+		return averageVolumn;
+	}
+	public void setAverageVolumn(int averageVolumn) {
+		this.averageVolumn = averageVolumn;
+	}
+	public double getAveragePrice() {
+		return averagePrice;
+	}
+	public void setAveragePrice(double averagePrice) {
+		this.averagePrice = averagePrice;
+	}	
+}
