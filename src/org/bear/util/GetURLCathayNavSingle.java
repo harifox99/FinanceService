9369a6d6052ba1b8353274f06cf40a22fb0e0a7e
bear <aluba0504@gmail.com>
2016-01-31 02:42:01 +0000

    修改每股淨值的URL

--- src/org/bear/util/GetURLCathayNavSingle.java
+++ src/org/bear/util/GetURLCathayNavSingle.java
@@ -3,9 +3,9 @@ package org.bear.util;
 public class GetURLCathayNavSingle extends GetURLContentBase {
 	public GetURLCathayNavSingle(String stockID, boolean isYear)
 	{
-		urlHeaderSeason = "";
-		urlHeaderYear = "http://dj.mybank.com.tw/z/zc/zcr/zcra/zcra0_";
-		urlFooter = ".asp.htm";
+		urlHeaderSeason = "";		               
+		urlHeaderYear = "http://www.esunsec.com.tw/z/zc/zcr/zcra/zcra0_";
+		urlFooter = ".djhtm";
 		if (isYear)
 			this.urlString = urlHeaderYear + stockID + urlFooter;
 		else
