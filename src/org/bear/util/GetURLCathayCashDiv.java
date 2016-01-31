5fa1752a93f8ca3d9e4812ded6f18ed622f4a8bd
bear <aluba0504@gmail.com>
2016-01-31 02:41:00 +0000

    修改配股配息的URL

--- src/org/bear/util/GetURLCathayCashDiv.java
+++ src/org/bear/util/GetURLCathayCashDiv.java
@@ -11,8 +11,9 @@ public class GetURLCathayCashDiv extends GetURLContentBase
 {
 	public GetURLCathayCashDiv(String stockID)
 	{
-		urlHeaderYear = "http://dj.mybank.com.tw/z/zc/zcc/zcc_";
-		urlFooter = ".asp.htm";
-		this.urlString = urlHeaderYear + stockID + urlFooter;
+		//urlHeaderYear = "http://dj.mybank.com.tw/z/zc/zcc/zcc_";
+		urlHeaderYear = "http://www.esunsec.com.tw/z/zc/zcc/zcc.djhtm?A=";
+		//urlFooter = ".asp.htm";
+		this.urlString = urlHeaderYear + stockID;
 	}
 }
