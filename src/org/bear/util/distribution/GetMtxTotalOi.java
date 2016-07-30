eb8d6d862e4247d28648760c3d3b28101859eeee
bear <aluba0504@gmail.com>
2016-07-30 15:07:11 +0000

    擷取小台指未平倉餘額

---
+++ src/org/bear/util/distribution/GetMtxTotalOi.java
@@ -0,0 +1,40 @@
+package org.bear.util.distribution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.bear.parser.MtxTotalOiParser;
+import org.bear.util.HttpUtil;
+/**
+ * 擷取小台指未平倉餘額
+ * @author edward
+ *
+ */
+public class GetMtxTotalOi extends GetTaifexLot 
+{
+	public void getContent()
+	{		
+		MtxTotalOiParser parser = new MtxTotalOiParser();
+		String[] dateArray = date.split("/");
+		List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+		paramList.add(new BasicNameValuePair("datestart", date));
+		paramList.add(new BasicNameValuePair("DATA_DATE_Y", dateArray[0]));
+		paramList.add(new BasicNameValuePair("DATA_DATE_M", dateArray[1]));
+		paramList.add(new BasicNameValuePair("DATA_DATE_D", dateArray[2]));
+		paramList.add(new BasicNameValuePair("syear", dateArray[0]));
+		paramList.add(new BasicNameValuePair("smonth", dateArray[1]));
+		paramList.add(new BasicNameValuePair("sday", dateArray[2]));
+		paramList.add(new BasicNameValuePair("qtype", "2"));
+		paramList.add(new BasicNameValuePair("commodity_id", "MTX"));
+		paramList.add(new BasicNameValuePair("commodity_idt", "MTX"));
+		paramList.add(new BasicNameValuePair("dateaddcnt", "0"));	
+		String responseString = HttpUtil.send(url, paramList, 1, "UTF-8");
+		//System.out.println(responseString);
+		parser.setResponseString(responseString);
+		parser.setDao(dao);
+		parser.setDate(date);
+		parser.parse(4);
+	}
+}
