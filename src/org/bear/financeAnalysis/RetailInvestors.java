bd461006b292d6ec6fd052018106c4993fa6b0cf
bear <aluba0504@gmail.com>
2016-07-31 10:20:48 +0000

    計算散戶指標

---
+++ src/org/bear/financeAnalysis/RetailInvestors.java
@@ -0,0 +1,36 @@
+package org.bear.financeAnalysis;
+import java.util.List;
+
+import org.bear.dao.JuristicDailyReportDao;
+import org.bear.entity.JuristicDailyEntity;
+import org.bear.entity.RetailInvestorsEntity;
+import org.bear.util.StringUtil;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+/**
+ * 計算散戶指標
+ * @author edward
+ *
+ */
+public class RetailInvestors 
+{
+	ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+	JuristicDailyReportDao juristicDailyReportDao = (JuristicDailyReportDao)context.getBean("juristicDailyReportDao");
+	public List<RetailInvestorsEntity> getRetailIndex(int size)
+	{
+		List<RetailInvestorsEntity> listRetail = juristicDailyReportDao.findRetailInvestors(size);
+		for (int i = 0; i < listRetail.size(); i++)
+		{
+			//計算散戶指標
+			int diff = listRetail.get(i).getTotalMtx()-listRetail.get(i).getInstitutionalMtx();
+			double retailRate = (double)diff/listRetail.get(i).getTotalMtx()-1;
+			retailRate = retailRate * 100;
+			retailRate = StringUtil.setPointLength(retailRate);
+			listRetail.get(i).setRetailRate(retailRate);
+			//擷取大盤指數
+			JuristicDailyEntity entity = juristicDailyReportDao.findByDate(listRetail.get(i).getExchangeDate());
+			listRetail.get(i).setTwseIndex(entity.getTwseIndex());
+		}
+		return listRetail;
+	}
+}
