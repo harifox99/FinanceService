6d9791563853ca879dc30361ba119001693a94b5
bear <aluba0504@gmail.com>
2016-09-04 12:41:04 +0000

    Parser重寫

--- src/org/bear/parser/taiwanMacro/TwseIndex.java
+++ src/org/bear/parser/taiwanMacro/TwseIndex.java
@@ -1,110 +1,46 @@
 package org.bear.parser.taiwanMacro;
+import java.util.ArrayList;
 import java.util.List;
-import net.htmlparser.jericho.Element;
-import net.htmlparser.jericho.HTMLElementName;
-
-import org.bear.constant.FredIndexParameterMap;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.bear.dao.MacroEconomicDao;
-import org.bear.parser.EasyParserBase;
+import org.bear.parser.TwseIndexParser;
+import org.bear.util.HttpUtil;
+import org.bear.util.StringUtil;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * 台灣加權指數月資料
  * @author edward
  *
  */
-public class TwseIndex extends EasyParserBase
+public class TwseIndex
 {
-	private String url = "http://finance.yahoo.com/q/hp?s=%5ETWII&a=03&b=1&c=2015&d=10&e=31&f=2015&g=m";
 	MacroEconomicDao dao;
-	public MacroEconomicDao getDao() {
-		return dao;
-	}
 	public void setDao(MacroEconomicDao dao) {
 		this.dao = dao;
 	}
-	public void getContent()
+	public void getContent(String data)
 	{		
-		this.setUrl(url);
-		this.getConnection();
-		//System.out.print(responseString);
-		this.parse();		
+		String[] date = data.split("M");
+		String url = "http://www.twse.com.tw/ch/trading/indices/MI_5MINS_HIST/MI_5MINS_HIST.php";
+		List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+		//日期
+		paramList.add(new BasicNameValuePair("myear", StringUtil.convertChineseYear(date[0])));
+		paramList.add(new BasicNameValuePair("mmon", date[1]));
+		String responseString = HttpUtil.send(url, paramList, 1, "big5");
+		TwseIndexParser parser = new TwseIndexParser();
+		parser.setResponseString(responseString);
+		parser.setDao(dao);
+		parser.setDateString(data);
+		parser.parse(8);		
 	}
-	@Override
-	public void getTableContent(Element element) 
+	public static void main(String args[])
 	{
-		List<Element> trList = element.getAllElements(HTMLElementName.TR);
-		for (int i = 0; i < trList.size(); i++)
-		{
-			if (i == trList.size()-1)
-				break;
-			Element trElement = trList.get(i);
-			List<Element> tdList = trElement.getAllElements(HTMLElementName.TD);
-			Element resultElement = null;
-			String date = "";
-			for (int j = 0; j < tdList.size(); j++)
-			{				
-				resultElement = tdList.get(j);
-				String content = resultElement.getContent().toString().trim();
-				String[] dateArr = null;
-				
-				if (j == 0)
-				{					
-					dateArr = content.split(",");
-					date = dateArr[1].trim() + "-" + FredIndexParameterMap.MONTH.get(dateArr[0].subSequence(0, dateArr[0].indexOf(" "))) + "-01";					
-				}
-				else
-				{
-					try
-					{
-						int result;
-						if (j == 1)
-						{
-							result = dao.update("TwseOpen", content.replace(",", ""), date);
-							this.printDaoErrorMessage(result, content, date);
-							
-						}
-						else if (j == 2)
-						{
-							result = dao.update("TwseHigh", content.replace(",", ""), date);
-							this.printDaoErrorMessage(result, content, date);
-						}															
-						else if (j == 3)
-						{
-							result = dao.update("TwseLow", content.replace(",", ""), date);
-							this.printDaoErrorMessage(result, content, date);
-						}
-						else if (j == 4)
-						{
-							result = dao.update("TwseClose", content.replace(",", ""), date);
-							this.printDaoErrorMessage(result, content, date);
-						}				
-						else
-							break;			
-					}
-									
-					catch (Exception ex)
-					{
-						System.out.println(date);
-						ex.printStackTrace();
-					}
-				}
-			}
-		}
-	}
-	/**
-	 * DAO更新失敗列出錯誤訊息 (通常是資料太新，比如說到了7月，都有及時的收盤資訊，但是總經資訊不夠新，所以比較新的收盤資料找不到對應的總經資訊)
-	 * @param result
-	 * @param content
-	 * @param date
-	 */
-	private void printDaoErrorMessage(int result, String content, String date)
-	{
-		if (result <= 0)
-	    {					    	
-	    	System.out.println(content + ", " + date);
-	    }
-	}
-	public void parse()
-	{		
-		this.getTableContent(elementList.get(14));
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		MacroEconomicDao dao = (MacroEconomicDao)context.getBean("macroEconomicDao");
+		TwseIndex index = new TwseIndex();
+		index.setDao(dao);
+		index.getContent("2016M08");
 	}
 }
