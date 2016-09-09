af3e1b719ed950010c049d23b2061e3388e1e38e
bear <aluba0504@gmail.com>
2016-09-09 14:53:59 +0000

    新的Yahoo財經網站Parser，SP500NewIndex

--- src/org/bear/main/SP500Main.java
+++ src/org/bear/main/SP500Main.java
@@ -1,6 +1,6 @@
 package org.bear.main;
 import org.bear.parser.CrbParser;
-import org.bear.parser.SP500Index;
+import org.bear.parser.SP500NewIndex;
 import org.bear.parser.SP500PeParser;
 /**
  * §ïSP500Index
@@ -9,14 +9,15 @@ import org.bear.parser.SP500PeParser;
  * @author edward
  *
  */
-public class SP500Main {
+public class SP500Main 
+{
 	public static void main(String[] args) 
 	{	
 		SP500PeParser spParser = new SP500PeParser();
 		spParser.getConnection();
 		spParser.parse(0);
-		SP500Index index = new SP500Index();
-		index.getConnection();
+		SP500NewIndex index = new SP500NewIndex();
+		index.update();
 		String url;
 		url = "http://www.barchart.com/chart.php?sym=%24CRB&style=technical&template=&p=MO&d=X&sd=02%2F01%2F1993&ed=12%2F31%2F2002&size=L&log=0&t=LINE&v=0&g=1&evnt=1&late=1&o1=&o2=&o3=&sh=100&indicators=&addindicator=&submitted=1&fpage=&txtDate=12%2F31%2F2002#jump";
 		CrbParser crb = new CrbParser();
