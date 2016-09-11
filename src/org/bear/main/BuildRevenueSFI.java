7cd56bc303888045ab995dd6b5e5d62232816895
bear <aluba0504@gmail.com>
2016-09-11 09:35:14 +0000

    修改日期參數

--- src/org/bear/main/BuildRevenueSFI.java
+++ src/org/bear/main/BuildRevenueSFI.java
@@ -13,9 +13,9 @@ public class BuildRevenueSFI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String startYear = "2016";
-		String startMonth = "6";
+		String startMonth = "8";
 		String endYear = "2016";
-		String endMonth = "6";
+		String endMonth = "8";
 		GetSFIContent getContent;
 		ImportPriceSFI sfi = new ImportPriceSFI();
 		/* 上市/上櫃營收資訊 (公開資訊觀測站)，僅用startYear and startMonth */
