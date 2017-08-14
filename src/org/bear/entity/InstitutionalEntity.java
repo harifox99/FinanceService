a539b499758246921bef83bbadf6cf7608fdd8f6
bear <aluba0504@gmail.com>
2017-08-14 13:09:26 +0000

    儲存法人每日買賣超佔股本比資料

--- src/org/bear/entity/InstitutionalEntity.java
+++ src/org/bear/entity/InstitutionalEntity.java
@@ -1,54 +1,60 @@
 package org.bear.entity;
 
+import java.util.List;
+/**
+ * 儲存法人每日買賣超佔股本比資料
+ * @author edward
+ *
+ */
 public class InstitutionalEntity {
+	/**
+	 * 股票代碼
+	 */
 	String stockID;
-	double oneDay;
-	double twoDays;
-	double threeDays;
-	double fiveDays;
-	double tenDays;
-	double thirtyDays;
+	/**
+	 * 交易資訊
+	 */
+	List<Double> info;
+	/**
+	 * 註解
+	 */
+	String comment;
+	/**
+	 * 連續買賣超天數
+	 */
+	String consecutiveDays;
+	/**
+	 * 名稱
+	 */
+	String name;
 	public String getStockID() {
 		return stockID;
 	}
 	public void setStockID(String stockID) {
 		this.stockID = stockID;
 	}
-	public double getOneDay() {
-		return oneDay;
+	public List<Double> getInfo() {
+		return info;
 	}
-	public void setOneDay(double oneDay) {
-		this.oneDay = oneDay;
+	public void setInfo(List<Double> info) {
+		this.info = info;
 	}
-	public double getTwoDays() {
-		return twoDays;
+	public String getComment() {
+		return comment;
 	}
-	public void setTwoDays(double twoDays) {
-		this.twoDays = twoDays;
+	public void setComment(String comment) {
+		this.comment = comment;
 	}
-	public double getThreeDays() {
-		return threeDays;
+	public String getConsecutiveDays() {
+		return consecutiveDays;
 	}
-	public void setThreeDays(double threeDays) {
-		this.threeDays = threeDays;
+	public void setConsecutiveDays(String consecutiveDays) {
+		this.consecutiveDays = consecutiveDays;
 	}
-	public double getFiveDays() {
-		return fiveDays;
+	public String getName() {
+		return name;
 	}
-	public void setFiveDays(double fiveDays) {
-		this.fiveDays = fiveDays;
-	}
-	public double getTenDays() {
-		return tenDays;
-	}
-	public void setTenDays(double tenDays) {
-		this.tenDays = tenDays;
-	}
-	public double getThirtyDays() {
-		return thirtyDays;
-	}
-	public void setThirtyDays(double thirtyDays) {
-		this.thirtyDays = thirtyDays;
-	}
-	
+	public void setName(String name) {
+		this.name = name;
+	}	
 }
