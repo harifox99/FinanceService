b17ef5188e9a225c7601c30a8bf270ec3c069ef7
bear <aluba0504@gmail.com>
2017-08-26 01:02:13 +0000

    修改日期參數

--- src/org/bear/constant/FinancialReport.java
+++ src/org/bear/constant/FinancialReport.java
@@ -1,7 +1,7 @@
 package org.bear.constant;
 
 public class FinancialReport {
-	public static int expectedNum = 1;
+	public static int expectedNum = 2;
 	public static int sleepTime = 1000;
 	//證交所，櫃買三大法人買賣超前N(30)名
 	public static int maxNumber = 30;
