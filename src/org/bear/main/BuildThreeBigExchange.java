58292fc0229a5f26dba09fc1377abda87c4e82d4
bear <aluba0504@gmail.com>
2017-08-31 15:14:45 +0000

    修改日期參數

--- src/org/bear/main/BuildThreeBigExchange.java
+++ src/org/bear/main/BuildThreeBigExchange.java
@@ -30,7 +30,7 @@ public class BuildThreeBigExchange {
 	JuristicDailyReportDao juristicDailyReportDao = (JuristicDailyReportDao)context.getBean("juristicDailyReportDao");
 	public static void main(String[] args) 
 	{
-		String[] date = {"106/08/24"};
+		String[] date = {"106/08/31"};
 		for (int i = 0; i < date.length; i++)
 		{
 			//把民國轉換成西元
