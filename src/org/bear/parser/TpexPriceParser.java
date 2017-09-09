f8cc1ad3ca7348c69a52591fa540e1b87886afea
bear <aluba0504@gmail.com>
2017-09-09 09:12:42 +0000

    證交所&櫃買收盤價Parser

---
+++ src/org/bear/parser/TpexPriceParser.java
@@ -0,0 +1,65 @@
+package org.bear.parser;
+
+import java.util.HashMap;
+import java.util.List;
+
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.HTMLElementName;
+/**
+ * Âd¶R¦¬½L»ùParser
+ * @author edward
+ *
+ */
+public class TpexPriceParser extends EasyParserBase 
+{
+	HashMap<String, Double> hashPrice = new HashMap<String, Double>();
+	public HashMap<String, Double> getHashPrice() {
+		return hashPrice;
+	}
+	public void setHashPrice(HashMap<String, Double> hashPrice) {
+		this.hashPrice = hashPrice;
+	}
+	@Override
+	public void getTableContent(Element element) {
+		// TODO Auto-generated method stub
+		Element resultElement = null;
+		List<Element> trList = element.getAllElements(HTMLElementName.TR);
+		for (int i = 2; i < trList.size(); i++)
+		{
+			Element trElement = trList.get(i);
+			List<Element> tdList = trElement.getAllElements(HTMLElementName.TD);
+			String stockID = "";
+			for (int j = 0; j < tdList.size(); j++)
+			{
+				resultElement = tdList.get(j);
+				String content = resultElement.getContent().toString();	
+				if (j == 0)
+					stockID = content.trim();
+				else if (j == 2)
+				{
+					double price;
+					if (content.contains("--"))
+					{
+						price = -1;
+						hashPrice.put(stockID, price);
+					}
+					else if (content.contains(","))
+					{
+						content = content.replace(",", "");
+						price = Double.parseDouble(content);
+						hashPrice.put(stockID, price);
+					}
+					else
+					{
+						price = Double.parseDouble(content);
+						hashPrice.put(stockID, price);
+					}
+				}
+			}
+		}			
+	}
+	public void parse(int index)
+	{		
+		this.getTableContent(elementList.get(index));
+	}	
+}
