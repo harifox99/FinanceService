fa334ce09117a3560d7535402db5f0797945b311
bear <aluba0504@gmail.com>
2017-10-01 04:35:17 +0000

    修正日期參數

--- src/org/bear/main/BuildThreeBigExchange.java
+++ src/org/bear/main/BuildThreeBigExchange.java
@@ -30,7 +30,7 @@ public class BuildThreeBigExchange {
 	JuristicDailyReportDao juristicDailyReportDao = (JuristicDailyReportDao)context.getBean("juristicDailyReportDao");
 	public static void main(String[] args) 
 	{
-		String[] date = {"106/09/20"};
+		String[] date = {"106/09/30"};
 		BuildThreeBigExchange trader = new BuildThreeBigExchange();
 		trader.update(date);
 	}
