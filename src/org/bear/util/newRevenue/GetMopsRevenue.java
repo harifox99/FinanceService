bd90fcd96c08bc264d9e8d670c7df2604c8ddade
bear <aluba0504@gmail.com>
2018-03-12 13:30:20 +0000

    減少重試次數

--- src/org/bear/util/newRevenue/GetMopsRevenue.java
+++ src/org/bear/util/newRevenue/GetMopsRevenue.java
@@ -33,7 +33,7 @@ public class GetMopsRevenue implements GetSFIContent {
 		paramList.add(new BasicNameValuePair("firstin", "true"));
 		boolean isSuccessful = false;
 		int loopIndex = 0;
-		while (isSuccessful == false && loopIndex++ < 15)
+		while (isSuccessful == false && loopIndex++ < 10)
 		{
 			String responseString = null;
 			try
