6a785f43909898b3721a054eae6d9ee440543762
bear <aluba0504@gmail.com>
2019-02-14 11:55:34 +0000

    修改日期參數

--- src/org/bear/main/BuildThreeBigExchange.java
+++ src/org/bear/main/BuildThreeBigExchange.java
@@ -101,7 +101,7 @@ public class BuildThreeBigExchange {
 				         "106/12/25", "106/12/26", "106/12/27", "106/12/28", "106/12/29",
 				         "107/01/02", "107/01/03", "107/01/04", "107/01/05"
 				         };*/
-		String[] date = {"107/10/11"};
+		String[] date = {"108/01/21"};
 		BuildThreeBigExchange trader = new BuildThreeBigExchange();
 		trader.update(date);
 		
