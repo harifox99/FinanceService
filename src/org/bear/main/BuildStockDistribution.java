087c3d33aa9b9079a844c63b849eb35f4338e445
bear <aluba0504@gmail.com>
2019-06-29 03:16:04 +0000

    Change Date Parameter

--- src/org/bear/main/BuildStockDistribution.java
+++ src/org/bear/main/BuildStockDistribution.java
@@ -15,7 +15,7 @@ public class BuildStockDistribution extends ImportStockID
 	{
 		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 		StockDistributionDao stockDistributionDao = (StockDistributionDao)context.getBean("stockDistributionDao");
-		String[] dateString = {"20190531"};
+		String[] dateString = {"20190628"};
 		for (int i = 0; i < dateString.length; i++)
 		{
 			for (int j = 0; j < wrapperList.size(); j++)
