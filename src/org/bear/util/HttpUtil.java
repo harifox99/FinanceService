a8e10dc583472a97d6014b1ca54fe4d8a02fa12b
bear <aluba0504@gmail.com>
2020-07-02 15:05:32 +0000

    Add Http Get Methods

--- src/org/bear/util/HttpUtil.java
+++ src/org/bear/util/HttpUtil.java
@@ -1,21 +1,24 @@
 package org.bear.util;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.net.*;
 import java.util.*;
-import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
+
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 /**
  * HTTP POST Utility
  * @author edward
@@ -31,6 +34,8 @@ public class HttpUtil {
 	 * @return response content
 	 * @throws IOException connection problem/aborted
 	 */
+	private String proxyHost = null;
+	private Integer proxyPort = null;
 	public static String send(String url, Map<String, String> paramMap, int retryCount, String encoding) throws IOException
 	{
 		DefaultHttpClient httpClinet = new DefaultHttpClient();
@@ -285,6 +290,105 @@ public class HttpUtil {
 		}
 		
 		return responseString;
+	}
+	/**
+	 * Http Get
+	 * @param url
+	 * @param charset
+	 * @return
+	 * @throws Exception
+	 */
+	public String doGet(String url, String charset) throws Exception 
+	{
+		URL localURL = new URL(url);
+		URLConnection connection = this.openConnection(localURL);
+		HttpURLConnection httpURLConnection = (HttpURLConnection)connection;
+		httpURLConnection.setRequestProperty("Accept-Charset", charset);
+		httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		InputStream inputStream = null;
+		InputStreamReader inputStreamReader = null;
+		BufferedReader reader = null;
+		StringBuffer resultBuffer = new StringBuffer();
+		String tempLine = null;
+		//響應失敗
+		if (httpURLConnection.getResponseCode() >= 300) 
+		{
+			throw new Exception("HTTP Request is not success, Response code is "  + httpURLConnection.getResponseCode());
+		}
+		try 
+		{
+			inputStream = httpURLConnection.getInputStream();
+			inputStreamReader = new InputStreamReader(inputStream);
+			reader = new BufferedReader(inputStreamReader);
+			while ((tempLine = reader.readLine()) != null) 
+			{
+				resultBuffer.append(tempLine);
+			}
+		} 
+		finally 
+		{
+			if (reader != null) 
+			{
+				reader.close();
+			}
+			if (inputStreamReader != null) 
+			{
+				inputStreamReader.close();
+			}
+			if (inputStream != null) 
+			{
+				inputStream.close();
+			}
+		}
+		return resultBuffer.toString();
+	}
+	private URLConnection openConnection(URL localURL) throws IOException 
+	{
+		URLConnection connection;
+		if (proxyHost != null && proxyPort != null) 
+		{
+			Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
+			connection = localURL.openConnection(proxy);
+		} 
+		else 
+		{
+			connection = localURL.openConnection();
+		}
+		return connection;
+	}
+	/**
+	 * Http Get
+	 * @param url
+	 * @return
+	 */
+	public static String httpGet(String url, String charset)
+	{
+		if (charset == null)
+			charset = "UTF-8";
+		try
+		{
+			DefaultHttpClient demo = new DefaultHttpClient();
+			demo.getParams().setParameter("http.protocol.content-charset", charset);
+		    // Get Request Example，取得 google 查詢 httpclient 的結果
+		    HttpGet httpGet = new HttpGet(url);
+			HttpResponse response = demo.execute(httpGet);
+			String responseString = EntityUtils.toString(response.getEntity());
+			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) 
+			{
+		    	// 如果回傳是 200 OK 的話才輸出
+				//System.out.println(responseString);
+			} 
+			else 
+			{
+		        System.out.println(response.getStatusLine());
+			}
+			return responseString;
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return null;
 	}
 }
 
