9388f7567adc2cd1dae8691b7fef1724285680b4
bear <aluba0504@gmail.com>
2021-11-14 01:57:20 +0000

    修改Parser，因為網頁內容異動

--- src/org/bear/kd/GoodInfoRequest.java
+++ src/org/bear/kd/GoodInfoRequest.java
@@ -2,10 +2,9 @@ package org.bear.kd;
 
 import java.io.*;
 import java.net.URL;
-import java.net.URLEncoder;
 import javax.net.ssl.*;
-import org.bear.parser.GoodInfoParser;
 
+import org.bear.parser.GoodInfoParser;
 /**
  * GoodInfo KD Parser
  * @author capital20180413
@@ -19,40 +18,16 @@ public class GoodInfoRequest
 		try
 		{
 			StringBuffer content = new StringBuffer();
-			//SSLContext sslContext = SSLContext.getInstance("TLSv1.2"); 
-			//sslContext.init(null, null, new SecureRandom());
 			SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-			String MARKET_CAT = URLEncoder.encode("智慧選股", "UTF-8");
-			String INDUSTRY_CAT = URLEncoder.encode("日KD低於20黃金交叉@@日KD相互交叉@@KD低於20黃金交叉", "UTF-8");
 			URL url;
 			if (isDay)
-			{
-				//url = new URL("https://goodinfo.tw/StockInfo/StockList.asp?MARKET_CAT=%E6%99%BA%E6%85%A7%E9%81%B8%E8%82%A1&INDUSTRY_CAT=%E6%97%A5KD%E4%BD%8E%E6%96%BC20%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89%40%40%E6%97%A5KD%E7%9B%B8%E4%BA%92%E4%BA%A4%E5%8F%89%40%40KD%E4%BD%8E%E6%96%BC20%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89&SHEET=KD%E6%8C%87%E6%A8%99&SHEET2=%E6%97%A5%2F%E9%80%B1%2F%E6%9C%88&RPT_TIME=");
-				url = new URL("https://goodinfo.tw/StockInfo/StockList.asp?RPT_TIME=&MARKET_CAT=%E6%99%BA%E6%85%A7%E9%81%B8%E8%82%A1&INDUSTRY_CAT=%E6%97%A5KD%E4%BD%8E%E6%96%BC20%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89%40%40%E6%97%A5KD%E7%9B%B8%E4%BA%92%E4%BA%A4%E5%8F%89%40%40KD%E4%BD%8E%E6%96%BC20%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89");								
-			}
+				url = new URL("https://goodinfo.tw/StockInfo/StockList.asp?MARKET_CAT=%E6%99%BA%E6%85%A7%E9%81%B8%E8%82%A1&INDUSTRY_CAT=%E6%97%A5KD%E4%BD%8E%E6%96%BC20%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89%40%40%E6%97%A5KD%E7%9B%B8%E4%BA%92%E4%BA%A4%E5%8F%89%40%40KD%E4%BD%8E%E6%96%BC20%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89&SHEET=KD%E6%8C%87%E6%A8%99&SHEET2=%E6%97%A5%2F%E9%80%B1%2F%E6%9C%88&RPT_TIME=");
 			else
-			{
-				//url = new URL("https://goodinfo.tw/StockInfo/StockList.asp?MARKET_CAT=%E6%99%BA%E6%85%A7%E9%81%B8%E8%82%A1&INDUSTRY_CAT=%E9%80%B1KD%E4%BD%8E%E6%96%BC20%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89%40%40%E9%80%B1KD%E7%9B%B8%E4%BA%92%E4%BA%A4%E5%8F%89%40%40KD%E4%BD%8E%E6%96%BC20%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89&SHEET=KD%E6%8C%87%E6%A8%99&SHEET2=%E6%97%A5%2F%E9%80%B1%2F%E6%9C%88&RPT_TIME=");
-				url = new URL("https://goodinfo.tw/StockInfo/StockList.asp?RPT_TIME=&MARKET_CAT=%E6%99%BA%E6%85%A7%E9%81%B8%E8%82%A1&INDUSTRY_CAT=%E9%80%B1KD%E4%BD%8E%E6%96%BC20%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89%40%40%E9%80%B1KD%E7%9B%B8%E4%BA%92%E4%BA%A4%E5%8F%89%40%40KD%E4%BD%8E%E6%96%BC20%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89");
-			}
+				url = new URL("https://goodinfo.tw/StockInfo/StockList.asp?MARKET_CAT=%E6%99%BA%E6%85%A7%E9%81%B8%E8%82%A1&INDUSTRY_CAT=%E9%80%B1KD%E4%BD%8E%E6%96%BC20%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89%40%40%E9%80%B1KD%E7%9B%B8%E4%BA%92%E4%BA%A4%E5%8F%89%40%40KD%E4%BD%8E%E6%96%BC20%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89&SHEET=KD%E6%8C%87%E6%A8%99&SHEET2=%E6%97%A5%2F%E9%80%B1%2F%E6%9C%88&RPT_TIME=");
 			HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
-			conn.setRequestMethod("POST");
-			conn.setDoInput(true);
-			conn.setDoOutput(true); 
-			//conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			//conn.setRequestProperty("Referer", "https://goodinfo.tw/StockInfo/StockList.asp?RPT_TIME=&MARKET_CAT=%E6%99%BA%E6%85%A7%E9%81%B8%E8%82%A1&INDUSTRY_CAT=%E6%97%A5KD%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89%40%40%E6%97%A5KD%E7%9B%B8%E4%BA%92%E4%BA%A4%E5%8F%89%40%40KD%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89");
-			conn.setRequestProperty("Referer", "https://goodinfo.tw/StockInfo/StockList.asp?RPT_TIME=&MARKET_CAT=%E6%99%BA%E6%85%A7%E9%81%B8%E8%82%A1&INDUSTRY_CAT=%E6%97%A5KD+20%7E50%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89%40%40%E6%97%A5KD%E7%9B%B8%E4%BA%92%E4%BA%A4%E5%8F%89%40%40KD+20%7E50%E9%BB%83%E9%87%91%E4%BA%A4%E5%8F%89");
-			//conn.setRequestProperty("cookie", "_ga=GA1.2.795921985.1520179488; __gads=ID=944f3deb3d244b7c:T=1520179488:S=ALNI_Ma-xj15q7lM5grfZKetSJM6376OkA; CLIENT%5FID=20180414160428687%5F220%2E135%2E163%2E10; SCREEN_SIZE=WIDTH=1366&HEIGHT=768; _gid=GA1.2.25478762.1525869105; _gat=1");
-			conn.setRequestProperty("cookie", "CLIENT%5FID=20210520224939328%5F114%2E136%2E119%2E190; SCREEN_SIZE=WIDTH=1280&HEIGHT=800; IS_TOUCH_DEVICE=F; _ga=GA1.2.1588212926.1621522184; __gads=ID=7645a22a2db0e8ed-22c857fbcac800e3:T=1621522185:S=ALNI_MagfIum2QZwKpTkcHoAtOYVOeaLsg; _gid=GA1.2.1947197764.1621993931; _gat=1");
-			//conn.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36");
-			  conn.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0");
+			conn.setRequestProperty("cookie", "_ga=GA1.2.795921985.1520179488; __gads=ID=944f3deb3d244b7c:T=1520179488:S=ALNI_Ma-xj15q7lM5grfZKetSJM6376OkA; CLIENT%5FID=20180414160428687%5F220%2E135%2E163%2E10; SCREEN_SIZE=WIDTH=1366&HEIGHT=768; _gid=GA1.2.25478762.1525869105; _gat=1");
+			conn.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36");
 			conn.setSSLSocketFactory(sslsocketfactory);
-			DataOutputStream dos=new DataOutputStream(conn.getOutputStream());
-			String postString = "RPT_TIME=&MARKET_CAT=" + MARKET_CAT + "&INDUSTRY_CAT=" + INDUSTRY_CAT;
-			System.out.println(postString);
-			dos.writeBytes(postString);
-			dos.flush();
-			dos.close();
 			InputStream inputstream = conn.getInputStream();
 			InputStreamReader inputstreamreader = new InputStreamReader(inputstream, "UTF-8");
 			BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
@@ -72,7 +47,7 @@ public class GoodInfoRequest
 	public static void main(String[] args) throws Exception
     {
 		GoodInfoRequest request = new GoodInfoRequest();
-		request.conn(true, "2021/05/25");
-		request.conn(false, "2021/05/25");
+		request.conn(true, "2021/11/12");
+		request.conn(false, "2021/11/12");
     }
 }
