348bdf7aa72be88dbea83a8e98ba61c0c15eace0
bear <aluba0504@gmail.com>
2022-03-13 09:05:07 +0000

    修改Parser參數

--- src/org/bear/parser/CashFlowsParserCathay.java
+++ src/org/bear/parser/CashFlowsParserCathay.java
@@ -52,11 +52,11 @@ public class CashFlowsParserCathay extends CashFlowsParserYam implements Parser
 	{
 		String rowData[] = new String[dataLength];
 		Element resultElement = null;
-		List<Element> trList = element.getAllElements(HTMLElementName.TR);
+		List<Element> trList = element.getAllElements(HTMLElementName.DIV);
 		for (int i = 0; i < trList.size(); i++)
 		{
 			Element trElement = trList.get(i);
-			List<Element> tdList = trElement.getAllElements(HTMLElementName.TD);
+			List<Element> tdList = trElement.getAllElements(HTMLElementName.SPAN);
 			for (int j = 0; j < tdList.size(); j++)
 			{
 				if (j == 0)
