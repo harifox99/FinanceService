600370ec3a26f19157bf464f9730a55f49ba33ce
bear <aluba0504@gmail.com>
2023-09-14 16:24:19 +0000

    修改股票種類StockType.java

---
+++ src/org/bear/entity/StockType.java
@@ -0,0 +1,18 @@
+package org.bear.entity;
+public class StockType 
+{
+	int typeId;
+	String typeName;
+	public int getTypeId() {
+		return typeId;
+	}
+	public void setTypeId(int typeId) {
+		this.typeId = typeId;
+	}
+	public String getTypeName() {
+		return typeName;
+	}
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}	
+}
