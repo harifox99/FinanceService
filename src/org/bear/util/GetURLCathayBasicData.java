11358647f70c4fd84d555b997d6594d3380012a6
bear <aluba0504@gmail.com>
2023-09-14 16:25:31 +0000

    http->https

--- src/org/bear/util/GetURLCathayBasicData.java
+++ src/org/bear/util/GetURLCathayBasicData.java
@@ -14,7 +14,7 @@ public class GetURLCathayBasicData extends GetURLContentBase
 		urlHeaderSeason = "";
 		//urlHeaderYear = "http://dj.mybank.com.tw/z/zc/zca/zca_";
 		//urlFooter = ".asp.htm";
-		urlHeaderYear = "http://sjmain.esunsec.com.tw/z/zc/zca/zca_";
+		urlHeaderYear = "https://sjmain.esunsec.com.tw/z/zc/zca/zca_";
 		//urlHeaderYear = "https://www.esunsec.com.tw/tw-stock/z/zc/zca/zca.djhtm?A=";
 		//urlHeaderYear = "http://www.esunsec.com.tw/z/zc/zca/zca.djhtm?A=";		
 		urlFooter = ".djhtm";
