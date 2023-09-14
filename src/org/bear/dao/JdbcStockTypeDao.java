4d3e8c3ef4c324255e4d98c8b52a563585185c4a
bear <aluba0504@gmail.com>
2023-09-14 16:22:38 +0000

    修改股票種類StockType.java

--- src/org/bear/dao/JdbcStockTypeDao.java
+++ src/org/bear/dao/JdbcStockTypeDao.java
@@ -1,6 +1,7 @@
 package org.bear.dao;
-
 import java.util.List;
+import org.bear.entity.StockType;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
 
 public class JdbcStockTypeDao extends SimpleJdbcDaoSupport implements StockTypeDao 
@@ -10,6 +11,14 @@ public class JdbcStockTypeDao extends SimpleJdbcDaoSupport implements StockTypeD
 		// TODO Auto-generated method stub
 		String sql = "select typeId from stockType";
 		List<Integer> data = (List<Integer>)getJdbcTemplate().queryForList(sql, Integer.class);
+		return data;
+	}
+
+	@Override
+	public List<StockType> findFullData() {
+		String sql = "select * from stockType";
+		List<StockType> data = 
+		this.getSimpleJdbcTemplate().query(sql, BeanPropertyRowMapper.newInstance(StockType.class));
 		return data;
 	}
 
--- src/org/bear/dao/StockTypeDao.java
+++ src/org/bear/dao/StockTypeDao.java
@@ -2,6 +2,9 @@ package org.bear.dao;
 
 import java.util.List;
 
+import org.bear.entity.StockType;
+
 public interface StockTypeDao {
 	public List<Integer> findAllData();
+	public List<StockType> findFullData();
 }
