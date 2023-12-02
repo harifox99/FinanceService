863de89ab1b665d105204e78c4569715d4fd9a83
bear <aluba0504@gmail.com>
2023-12-02 02:56:20 +0000

    修改參數

--- src/org/bear/constant/FinancialReport.java
+++ src/org/bear/constant/FinancialReport.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 public class FinancialReport 
 {
-	public static int expectedNum = 1;
+	public static int expectedNum = 3;
 	public static int sleepTime = 1000;
 	//證交所，櫃買三大法人買賣超前N(30)名
 	public static int maxNumber = 30;
