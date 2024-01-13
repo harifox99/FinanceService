2f5edbd4e74ed78060b3c6cb2e7905a94d282a9a
bear <aluba0504@gmail.com>
2024-01-13 05:38:34 +0000

    Refactoring

--- src/org/bear/datainput/GetSFIContent.java
+++ src/org/bear/datainput/GetSFIContent.java
@@ -1,5 +1,6 @@
 package org.bear.datainput;
 
-public interface GetSFIContent {
+public interface GetSFIContent
+{
 	public void getContent(String stockID, String stockName, String startYear, String startMonth, String endYear, String endMonth);
 }
