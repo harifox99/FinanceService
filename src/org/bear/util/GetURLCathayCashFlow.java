0664fd29b0dc84349f750b94ff61c048d990325b
bear <aluba0504@gmail.com>
2024-03-31 02:05:52 +0000

    資料來源從玉山->國泰證券

--- src/org/bear/util/GetURLCathayCashFlow.java
+++ src/org/bear/util/GetURLCathayCashFlow.java
@@ -9,8 +9,8 @@ public class GetURLCathayCashFlow extends GetURLContentBase
 	//public final String cashFlowsUrlHeader2 = "http://dj.mybank.com.tw/z/zc/zc3/zc3_1101.djhtml";
 	public GetURLCathayCashFlow(String stockID, boolean isYear)
 	{
-		urlHeaderSeason = "http://sjmain.esunsec.com.tw/z/zc/zc3/zc3.djhtm?A=";
-		urlHeaderYear = "http://sjmain.esunsec.com.tw/z/zc/zc3/zc3a.djhtm?A=";	
+		urlHeaderSeason = "https://djinfo.cathaysec.com.tw/Z/ZC/ZC3/ZC3.DJHTM?A=";
+		urlHeaderYear = "https://djinfo.cathaysec.com.tw/z/zc/zc30.djhtm?b=Y&a=";	
 		urlFooter = "";
 		if (isYear)
 			this.urlString = urlHeaderYear + stockID + urlFooter;
