d1c907f4e2353224a062f69b96f64e043c1457d8
bear <aluba0504@gmail.com>
2024-05-05 10:13:31 +0000

    HTTP Post With Header，修改Parser

--- src/org/bear/util/distribution/GetMtxTotalOi.java
+++ src/org/bear/util/distribution/GetMtxTotalOi.java
@@ -1,12 +1,5 @@
 package org.bear.util.distribution;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 import org.bear.parser.MtxTotalOiParser;
-import org.bear.util.HttpUtil;
 /**
  * 擷取小台指未平倉餘額
  * @author edward
@@ -16,18 +9,8 @@ public class GetMtxTotalOi extends GetTaifexLot
 {
 	public void getContent()
 	{		
-		MtxTotalOiParser parser = new MtxTotalOiParser();
-		List<NameValuePair> paramList = new ArrayList<NameValuePair>();
-		paramList.add(new BasicNameValuePair("queryDate", date));
-		paramList.add(new BasicNameValuePair("queryType", "2"));
-		paramList.add(new BasicNameValuePair("marketCode", "0"));	
-		paramList.add(new BasicNameValuePair("commodity_id", "MTX"));	
-		paramList.add(new BasicNameValuePair("commodity_idt", "MTX"));	
-		String responseString = HttpUtil.send(url, paramList, 1, "UTF-8");
-		//System.out.println(responseString);
-		parser.setResponseString(responseString);
-		parser.setDao(dao);
-		parser.setDate(date);
-		parser.parse(4);
+		HttpPostWithHeader postHeader = new HttpPostWithHeader();
+		MtxTotalOiParser parser = new MtxTotalOiParser();	
+		postHeader.getContent(url, date, dao, parser);
 	}
 }
