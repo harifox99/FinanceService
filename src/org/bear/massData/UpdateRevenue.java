c706c5e9a522a485792f8baf348c42c84944d93f
bear <aluba0504@gmail.com>
2024-11-09 13:56:27 +0000

    UpdateRevenue (大批營收更新)

---
+++ src/org/bear/massData/UpdateRevenue.java
@@ -0,0 +1,73 @@
+package org.bear.massData;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.bear.dao.RevenueDao;
+import org.bear.entity.RevenueEntity;
+import org.bear.util.HttpUtil;
+import org.bear.util.StringUtil;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+/**
+ * 大批營收更新
+ * @author bear
+ *
+ */
+public class UpdateRevenue 
+{
+	String url = "https://mops.twse.com.tw/server-java/FileDownLoad";
+	String parameters;
+	ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+	RevenueDao dao = (RevenueDao)context.getBean("revenueDao");
+	public List<RevenueEntity> entityList = new ArrayList<RevenueEntity>();	
+	public void getData()
+	{
+		String url = "https://mops.twse.com.tw/server-java/FileDownLoad";
+		String parameters = "step=9&functionName=show_file2&filePath=%2Ft21%2Fsii%2F&fileName=t21sc03_113_10.csv";				
+		HttpUtil httpUtil = new HttpUtil();		
+		List<String> listData = httpUtil.httpPostList(url, parameters, "UTF-8");
+		try
+		{
+			for (int i = 1; i < 5; i++)
+			{
+				String[] data = listData.get(i).split("\",\"");
+				RevenueEntity entity = new RevenueEntity();
+				String[] dateString = data[1].split("/");
+				String year = StringUtil.convertYear(dateString[0]);
+				String month = StringUtil.addZeroMonth(dateString[1]);
+				entity.setStockID(data[2]);
+				entity.setRevenue(Integer.parseInt(data[5]));
+				entity.setLastRevenue(Integer.parseInt(data[6]));
+				entity.setAccumulation(Long.parseLong(data[10]));
+				entity.setLastAccumulation(Long.parseLong(data[11]));
+				SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM");						
+				Date date = dateFormat.parse(year + "-" + month);
+				entity.setYearMonth(date);
+				entityList.add(entity);	
+			}
+			dao.insertBatch(entityList);				
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}		
+	}
+	public static void main(String[] args)
+	{
+		UpdateRevenue revenue = new UpdateRevenue();
+		revenue.getData();
+	}
+	public String getParameters() {
+		return parameters;
+	}
+	public void setParameters(String parameters) {
+		this.parameters = parameters;
+	}
+	public RevenueDao getDao() {
+		return dao;
+	}
+	public void setDao(RevenueDao dao) {
+		this.dao = dao;
+	}	
+}
