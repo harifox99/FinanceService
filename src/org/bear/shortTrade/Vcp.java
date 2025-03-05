a6430cc05dbe720d5a4db3fecf38648aef178c1b
bear <aluba0504@gmail.com>
2025-03-05 13:32:59 +0000

    VCP指標

---
+++ src/org/bear/shortTrade/Vcp.java
@@ -0,0 +1,60 @@
+package org.bear.shortTrade;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.bear.parser.file.FileParser;
+import org.bear.util.GetURLContent;
+/**
+ * VCP
+ * @author bear
+ *
+ */
+public class Vcp 
+{
+	String urlString = "https://stock.pingponglife.tw/vcp/export?date=";
+	Set<String> sets = new HashSet<String>();
+	
+	public void getContent()
+	{		
+		try
+		{
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			java.util.Date today = new Date();        
+			String dateString = dateFormat.format(today);
+			GetURLContent content = new GetURLContent(urlString + dateString);
+			FileParser fileParser = new FileParser();
+			List<String> listData = fileParser.getResponse(content.getContent("UTF-8"));
+			for (int i = 0; i < listData.size(); i++)
+			{
+				String[] stocks = listData.get(i).split(".tw");
+				String stockId = stocks[0];
+				sets.add(stockId);
+			}			
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();	
+		}
+	}
+	
+
+	public Set<String> getSets() {
+		return sets;
+	}
+
+
+	public void setSets(Set<String> sets) {
+		this.sets = sets;
+	}
+
+
+	public static void main(String[] args)
+	{
+		Vcp vcp = new Vcp();
+		vcp.getContent();
+	}
+
+}
