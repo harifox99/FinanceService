8047b0175a874c247979d734c78fbc02e56c39e6
harifox99 <aluba0504@gmail.com>
2026-09-01 11:19:16 +0800

    AI開發的集保股權分配表

--- src/org/bear/main/BuildStockDistribution.java
+++ src/org/bear/main/BuildStockDistribution.java
@@ -1,61 +1,76 @@
 package org.bear.main;
-import org.bear.dao.*;
-import org.bear.datainput.ImportStockID;
-import org.bear.exception.TdccException;
-import org.bear.token.CsrfToken;
-import org.bear.util.distribution.StockDistribution;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class BuildStockDistribution extends ImportStockID
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import org.bear.dao.StockDistributionDao;
+import org.bear.parser.distribution.StockDistributionParser;
+import org.jsoup.Connection;
+/**
+ *  AI開發的集保股權分配表
+ */
+public class BuildStockDistribution
 {
-	public static void main(String[] args) throws InterruptedException 
+	private static final String URL = "https://www.tdcc.com.tw/portal/zh/smWeb/qryStock";
+	public static void main(String[] args) throws Exception
 	{
-		new BuildStockDistribution();
+		BuildStockDistribution distribution = new BuildStockDistribution();
+		distribution.getData("20260828", "StockDistribution", true);
 	}
-	public BuildStockDistribution() throws InterruptedException
+	public void getData(String dateString, String tableName, boolean isCurrentMonth)
 	{
 		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-		StockDistributionDao stockDistributionDao = (StockDistributionDao)context.getBean("stockDistributionDao");
-		String[] dateString = {"20220930"};										
-		for (int i = 0; i < dateString.length; i++)
+		StockDistributionDao dao = (StockDistributionDao)context.getBean("stockDistributionDao");
+		List<String> stockList = new ArrayList<String>();		
+		String scaDate = dateString;
+		try
 		{
-			CsrfToken token = new CsrfToken("https://www.tdcc.com.tw/portal/zh/smWeb/qryStock");	
-			for (int j = 0; j < wrapperList.size(); j++)
+			String stockNo = "";
+			// Step0 取得股票列表 
+			BufferedReader reader = new BufferedReader(new FileReader("C:/Users/bear/Desktop/StockList.txt"));
+			String readData;			
+			while((readData = reader.readLine()) != null)
 			{
-				/*
-				if (!wrapperList.get(j).getStockID().equals("1235") &&
-					!wrapperList.get(j).getStockID().equals("3562") &&	
-					!wrapperList.get(j).getStockID().equals("4939") &&
-					!wrapperList.get(j).getStockID().equals("6128") &&
-					!wrapperList.get(j).getStockID().equals("6240") &&
-					!wrapperList.get(j).getStockID().equals("5392"))
-					continue;*/
-				String csrf_token = token.getCsrf_token();
-				while(true)
-				{
-					try
-					{
-						System.out.println("csrf_token: " + csrf_token);
-						System.out.println("StockID: " + wrapperList.get(j).getStockID() + ", " + j);						
-						StockDistribution stockDistribution = new StockDistribution();
-						stockDistribution.setDao(stockDistributionDao);
-						stockDistribution.setCurrentMonth(true);
-						stockDistribution.getContent(wrapperList.get(j).getStockID(), 
-						dateString[i].substring(0, 4), dateString[i].substring(4, 8), null, null, csrf_token);
-						if (csrf_token == null)
-							break;
-						else
-							continue;
-					}
-					catch (TdccException ex)
-					{
-						System.out.println("Zero Data!");
-						Thread.sleep(1000);						
-						//Do Nothing
-					}
-				}
+				stockList.add(readData.trim());
 			}
+			reader.close();		
+			for (int i = 0; i < stockList.size(); i++)
+			{
+				stockNo = stockList.get(i);
+        		// Step1 取得首頁
+        		Connection.Response response = Jsoup.connect(URL).method(Connection.Method.GET).userAgent("Mozilla/5.0").execute();
+        		Document doc = response.parse();
+        		// Step2 取得 Token
+        		String token = doc.select("#SYNCHRONIZER_TOKEN").attr("value");
+        		System.out.println("TOKEN=" + token);
+        
+        		// Step3 POST 查詢
+        		Connection.Response postResponse = Jsoup.connect(URL).method(Connection.Method.POST)
+        				.cookie("JSESSIONID", response.cookie("JSESSIONID")).userAgent("Mozilla/5.0")
+        				.data("SYNCHRONIZER_TOKEN", token).data("SYNCHRONIZER_URI", "/portal/zh/smWeb/qryStock")
+        				.data("method", "submit").data("firDate", scaDate).data("scaDate", scaDate).data("sqlMethod", "StockNo")
+        				.data("stockNo", stockNo).data("stockName", "").execute();
+        
+        		Document resultDoc = postResponse.parse();    
+        		System.out.println(resultDoc.title());
+        		// Debug
+        		System.out.println(resultDoc.outerHtml());
+        		StockDistributionParser parser = new StockDistributionParser();
+    			parser.setDao(dao);
+    			parser.setStockID(stockNo);
+    			parser.setCurrentMonth(isCurrentMonth);
+    			parser.setDateString(dateString);
+    			parser.setResponseString(resultDoc.outerHtml());
+    			parser.parse(1);	
+			}
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
 		}
 	}
 }
